29c434c07119cc08e38bd430129582b81a3d4708
Kevin Huck <khuck@cs.uoregon.edu>
2016-11-16 13:45:11 -0800

    Prevent sqrt(0) errors.

--- src/grid_fmm.cpp
+++ src/grid_fmm.cpp
@@ -704,9 +704,9 @@ void compute_ilist() {
 	std::array < std::vector<interaction_type>, geo::direction::count() > ilist_d0_bnd;
 	const real theta0 = opts.theta;
 	const auto theta = [](integer i0, integer j0, integer k0, integer i1, integer j1, integer k1) {
-		real tmp = std::sqrt((i0-i1)*(i0-i1)+(j0-j1)*(j0-j1)+(k0-k1)*(k0-k1));
+		real tmp = ((i0-i1)*(i0-i1)+(j0-j1)*(j0-j1)+(k0-k1)*(k0-k1));
 		if( tmp > 0.0 ) {
-			return 1.0 / tmp;
+			return 1.0 / (std::sqrt(tmp));
 		} else {
 			return 1.0e+10;
 		}
@@ -763,7 +763,7 @@ void compute_ilist() {
 							const real x = i0 - i1;
 							const real y = j0 - j1;
 							const real z = k0 - k1;
-							const real r = std::sqrt(x * x + y * y + z * z);
+							const real r = (x==y==z==0) ? 0 : (std::sqrt(x * x + y * y + z * z));
 							const real r3 = r * r * r;
 							v4sd four;
 							if (r > 0.0) {
