82aa3dc30b851bfbe1862908a6ba7ad8f931a958
pfandedd <David.Pfander@ipvs.uni-stuttgart.de>
2017-01-27 00:48:20 -0800

    faster node-level scaling experiments

--- tools/scaling-experiment-single-node.sh
+++ tools/scaling-experiment-single-node.sh
@@ -19,10 +19,12 @@ level=$2
 # set 1 (MCDRAM) or 0
 memory_type=$3
 threads=$4
-max_threads=$5
+threads_increment=$5
 
-while [[ (${threads} -le ${max_threads}) && (${threads} -le 68) ]]; do
-    srun numactl -m ${memory_type} ./knl-build/octotiger-Release/octotiger -Disableoutput -Problem=dwd -Max_level=${level} -Xscale=36.0 -Eos=wd -Angcon=1 -Stopstep=100 --hpx:threads=${threads} -Restart=restart${level}.chk --hpx:ini=hpx.stacks.small_size=0xC0000 -Ihpx.stacks.use_guard_pages=0 --hpx:print-bind --hpx:print-counter /threads{locality#*/total}/idle-rate  > results/${name}_N${SLURM_NNODES}_t${threads}_l${level}_m${memory_type} 2>&1
+# Add these in case of crashes: --hpx:ini=hpx.stacks.small_size=0xC0000 -Ihpx.stacks.use_guard_pages=0
+
+while [[ ${threads} -le 68 ]]; do
+    echo "srun numactl -m ${memory_type} ./knl-build/octotiger-Release/octotiger -Disableoutput -Problem=dwd -Max_level=${level} -Xscale=36.0 -Eos=wd -Angcon=1 -Stopstep=30 --hpx:threads=${threads} -Restart=restart${level}.chk --hpx:print-bind --hpx:print-counter /threads{locality#*/total}/idle-rate  > results/${name}_N${SLURM_NNODES}_t${threads}_l${level}_m${memory_type} 2>&1"
     echo ${threads}
-    let threads=threads+1
+    let threads=threads+${threads_increment}
 done
