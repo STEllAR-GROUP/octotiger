8b5d7d9792d6091836b683543892a9b688dfbb96
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-02-18 21:46:55 -0600

    Flattened one more loop

--- src/taylor.hpp
+++ src/taylor.hpp
@@ -379,6 +379,83 @@ public:
 template <int N, class T>
 taylor_consts taylor<N, T>::tc;
 
+constexpr integer to_aa[] = {
+    -1,
+     4,  7,  9
+};
+constexpr integer to_aaa[] = {
+    -1,
+    10, 16, 19
+};
+constexpr integer to_aaaa[] = {
+    -1,
+    20, 30, 34
+};
+
+constexpr integer to_aab[] = {
+    -1,
+    -1, -1, -1,
+    10, 11, 12, 16, 17, 19
+};
+constexpr integer to_abb[] = {
+    -1,
+    -1, -1, -1,
+    10, 13, 15, 16, 18, 19
+};
+constexpr integer to_aaab[] = {
+    -1,
+    -1, -1, -1,
+    20, 21, 22, 30, 31, 34
+};
+constexpr integer to_abbb[] = {
+    -1,
+    -1, -1, -1,
+    20, 26, 29, 30, 33, 34
+};
+constexpr integer to_aabb[] = {
+    -1,
+    -1, -1, -1,
+    20, 23, 25, 30, 32, 34
+};
+
+constexpr integer to_aabc[] = {
+    -1,
+    -1, -1, -1,
+    -1, -1, -1, -1, -1, -1,
+    20, 21, 22, 23, 24, 25, 30, 31, 32, 34
+};
+constexpr integer to_abbc[] = {
+    -1,
+    -1, -1, -1,
+    -1, -1, -1, -1, -1, -1,
+    20, 21, 22, 26, 27, 29, 30, 31, 33, 34
+};
+constexpr integer to_abcc[] = {
+    -1,
+    -1, -1, -1,
+    -1, -1, -1, -1, -1, -1,
+    20, 23, 25, 26, 28, 29, 30, 32, 33, 34
+};
+
+constexpr integer to_a[] = {
+   -1,
+    0,  1,  2,
+    0,  0,  0,  1,  1,  2,
+    0,  0,  0,  0,  0,  0,  1,  1,  1,  2
+};
+constexpr integer to_b[] = {
+   -1,
+   -1, -1, -1,
+    0,  1,  2,  1,  2,  2,
+    0,  0,  0,  1,  1,  2,  1,  1,  2,  2
+};
+constexpr integer to_c[] = {
+   -1,
+   -1, -1, -1,
+   -1, -1, -1, -1, -1, -1,
+    0,  1,  2,  1,  2,  2,  1,  2,  2,  2
+};
+
 template <>
 inline void taylor<5, simd_vector>::set_basis(const std::array<simd_vector, NDIM>& X) {
     constexpr integer N = 5;
@@ -445,26 +522,50 @@ inline void taylor<5, simd_vector>::set_basis(const std::array<simd_vector, NDIM
     }
 
     auto const d22 = 2.0 * d2;
-    for (integer a = 0; a != NDIM; a++) {
-        auto const Xad2 = X[a] * d2;
-        auto const Xad3 = X[a] * d3;
-        A(a, a) += d1;
-        A(a, a, a) += Xad2;
-        A(a, a, a, a) += Xad3 * X[a] + d22;
-        for (integer b = a; b != NDIM; b++) {
-            auto const Xabd3 = Xad3 * X[b];
-            auto const Xbd3 = X[b] * d3;
-            A(a, a, b) += X[b] * d2;
-            A(a, b, b) += Xad2;
-            A(a, a, a, b) += Xabd3;
-            A(a, b, b, b) += Xabd3;
-            A(a, a, b, b) += d2;
-            for (integer c = b; c != NDIM; c++) {
-                A(a, a, b, c) += Xbd3 * X[c];
-                A(a, b, b, c) += Xad3 * X[c];
-                A(a, b, c, c) += Xabd3;
-            }
-        }
+//     for (integer a = 0; a != NDIM; a++) {
+//         auto const Xad2 = X[a] * d2;
+//         auto const Xad3 = X[a] * d3;
+//         A(a, a) += d1;
+//         A(a, a, a) += Xad2;
+//         A(a, a, a, a) += Xad3 * X[a] + d22;
+//         for (integer b = a; b != NDIM; b++) {
+//             auto const Xabd3 = Xad3 * X[b];
+//             auto const Xbd3 = X[b] * d3;
+//             A(a, a, b) += X[b] * d2;
+//             A(a, b, b) += Xad2;
+//             A(a, a, a, b) += Xabd3;
+//             A(a, b, b, b) += Xabd3;
+//             A(a, a, b, b) += d2;
+//             for (integer c = b; c != NDIM; c++) {
+//                 A(a, a, b, c) += Xbd3 * X[c];
+//                 A(a, b, b, c) += Xad3 * X[c];
+//                 A(a, b, c, c) += Xabd3;
+//             }
+//         }
+//     }
+    for (integer i = taylor_sizes[0]; i != taylor_sizes[1]; ++i) {
+        A[to_aa[i]] += d1;
+        integer const to_a_idx = to_a[i];
+        A[to_aaa[i]] += X[to_a_idx] * d2;
+        A[to_aaaa[i]] += sqr(X[to_a_idx]) * d3 + d22;
+    }
+    for (integer i = taylor_sizes[1]; i != taylor_sizes[2]; ++i) {
+        integer const to_a_idx = to_a[i];
+        integer const to_b_idx = to_b[i];
+        auto const Xabd3 = X[to_a_idx] * X[to_b_idx] * d3;
+        A[to_aab[i]] += X[to_b_idx] * d2;
+        A[to_abb[i]] += X[to_a_idx] * d2;
+        A[to_aaab[i]] += Xabd3;
+        A[to_abbb[i]] += Xabd3;
+        A[to_aabb[i]] += d2;
+    }
+    for (integer i = taylor_sizes[2]; i != taylor_sizes[3]; ++i) {
+        integer const to_a_idx = to_a[i];
+        integer const to_c_idx = to_c[i];
+        auto const Xbd3 = X[to_b[i]] * d3;
+        A[to_aabc[i]] += Xbd3 * X[to_c_idx];
+        A[to_abbc[i]] += X[to_a_idx] * X[to_c_idx] * d3;
+        A[to_abcc[i]] += X[to_a_idx] * Xbd3;
     }
 
     // PROF_END;
