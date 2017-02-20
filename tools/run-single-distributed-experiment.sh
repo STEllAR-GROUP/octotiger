0a71f2ef83362b25f761ff1440b87863e454bee6
David Pfander <David.Pfander@ipvs.uni-stuttgart.de>
2017-02-20 13:20:55 +0100

    Another scaling script fix

--- tools/run-single-distributed-experiment.sh
+++ tools/run-single-distributed-experiment.sh
@@ -13,6 +13,6 @@ name=$1
 level=$2
 threads=$3
 
-srun numactl -m 1 ./mic-knl-gcc-build/octotiger-Release/octotiger -Disableoutput -Problem=dwd -Max_level=${level} -Xscale=36.0 -Eos=wd -Angcon=1 -Stopstep=100 --hpx:threads=${threads} -Restart=restart${level}.chk --hpx:ini=hpx.stacks.small_size=0xC0000 -Ihpx.stacks.use_guard_pages=0 --hpx:print-bind --hpx:print-counter /threads{locality#*/total}/idle-rate  > results/${name}_N${SLURM_NNODES}_t${threads}_l${level}_m1 2>&1
+srun numactl -m 1 ./mic-knl-gcc-build/octotiger-tcmalloc-Release/octotiger -Disableoutput -Problem=dwd -Max_level=${level} -Xscale=36.0 -Eos=wd -Angcon=1 -Stopstep=100 --hpx:threads=${threads} -Restart=restart${level}.chk --hpx:ini=hpx.stacks.small_size=0xC0000 -Ihpx.stacks.use_guard_pages=0 --hpx:print-bind --hpx:print-counter /threads{locality#*/total}/idle-rate  > results/${name}_N${SLURM_NNODES}_t${threads}_l${level}_m1 2>&1
 
-srun numactl -m 0 ./mic-knl-gcc-build/octotiger-Release/octotiger -Disableoutput -Problem=dwd -Max_level=${level} -Xscale=36.0 -Eos=wd -Angcon=1 -Stopstep=100 --hpx:threads=${threads} -Restart=restart${level}.chk --hpx:ini=hpx.stacks.small_size=0xC0000 -Ihpx.stacks.use_guard_pages=0 --hpx:print-bind --hpx:print-counter /threads{locality#*/total}/idle-rate  > results/${name}_N${SLURM_NNODES}_t${threads}_l${level}_m0 2>&1
+srun numactl -m 0 ./mic-knl-gcc-build/octotiger-tcmalloc-Release/octotiger -Disableoutput -Problem=dwd -Max_level=${level} -Xscale=36.0 -Eos=wd -Angcon=1 -Stopstep=100 --hpx:threads=${threads} -Restart=restart${level}.chk --hpx:ini=hpx.stacks.small_size=0xC0000 -Ihpx.stacks.use_guard_pages=0 --hpx:print-bind --hpx:print-counter /threads{locality#*/total}/idle-rate  > results/${name}_N${SLURM_NNODES}_t${threads}_l${level}_m0 2>&1
