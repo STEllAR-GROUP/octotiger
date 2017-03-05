b1329f4219d413e18e0886feac10a00243ffa975
Hartmut Kaiser <hartmut.kaiser@gmail.com>
2017-03-05 02:54:55 -0600

    Fixing array sizes
    # Conflicts:
    #	src/grid_fmm.cpp

--- src/grid_fmm.cpp
+++ src/grid_fmm.cpp
@@ -164,7 +164,7 @@ void grid::solve_gravity(gsolve_type type) {
     compute_expansions(type);
 }
 
-constexpr int to_ab_idx_map3[3][6] = {
+constexpr int to_ab_idx_map3[3][3] = {
     {  4,  5,  6  },
     {  5,  7,  8  },
     {  6,  8,  9  }
