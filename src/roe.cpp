041baed71fbea9347e5948104aa371b46618bb64
fire <fire@element.com>
2017-03-29 00:15:38 +0200

    Some inlining for roe

--- src/roe.cpp
+++ src/roe.cpp
@@ -21,32 +21,6 @@ const integer acr_i = sy_i;
 const integer sh1_i = sz_i;
 const integer sh2_i = egas_i;
 
-real ztwd_pressure(real d, real A, real B) {
-	const real x = pow(d / B, 1.0 / 3.0);
-	real p;
-	if (x < 0.01) {
-		p = 1.6 * A * pow(x, 5);
-	} else {
-		p = A * (x * (2.0 * x * x - 3.0) * sqrt(x * x + 1.0) + 3.0 * asinh(x));
-	}
-	return p;
-}
-
-real ztwd_enthalpy(real d, real A, real B) {
-	const real x = pow(d / B, 1.0 / 3.0);
-	real h;
-	if (x < 0.01) {
-		h = 4.0 * A / B * x * x;
-	} else {
-		h = 8.0 * A / B * (sqrt(x * x + 1.0) - 1.0);
-	}
-	return h;
-}
-
-real ztwd_energy(real d, real A, real B) {
-	return std::max(ztwd_enthalpy(d) * d - ztwd_pressure(d), real(0));
-}
-
 real ztwd_sound_speed(real d, real ei) {
 	const real A = physcon.A;
 	const real B = physcon.B;
