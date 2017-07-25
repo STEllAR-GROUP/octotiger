a9270577d045bb048f64fd952070383d96c9a3a2
Dominic Marcello <dmarce504@gmail.com>
2017-07-25 01:40:53 -0500

    Fixed bug in rho_move

--- src/grid.cpp
+++ src/grid.cpp
@@ -1282,7 +1282,7 @@ void grid::rho_move(real x) {
 				for (integer si = spc_i; si != NSPECIES + spc_i; ++si) {
 					U[si][hindex(i, j, k)] += w * U0[si][hindex(i + 1, j, k)];
 					U[si][hindex(i, j, k)] -= w * U0[si][hindex(i - 1, j, k)];
-					U[si][hindex(i, j, k)] = std::max(U0[si][hindex(i, j, k)], 0.0);
+					U[si][hindex(i, j, k)] = std::max(U[si][hindex(i, j, k)], 0.0);
 				}
 				U[rho_i][hindex(i, j, k)] = 0.0;
 				for (integer si = 0; si != NSPECIES; ++si) {
