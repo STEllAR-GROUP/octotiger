324a11e932ff18f8f3c9411370dbc50e1e78644a
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2018-01-12 15:57:05 +0100

    Added collection of assembler dump to test script

--- verification_and_scaling_test.sh
+++ verification_and_scaling_test.sh
@@ -5,10 +5,10 @@ check_results() {
 	# Loop through all result names
 	for result_name in "rho" "egas" "sx" "sy" "tau" "primary_core"; do
 		# | sed -e 's/^[ \t]*// removes all leading whitespaces
-		result_correct="$(cat "$1" | grep "$result_name" | sed -e 's/^[ \t]*//' | sed "s/$result_name//g")" 
-		result_actual="$(echo "$2" | grep "$result_name" | sed -e 's/^[ \t]*//' | sed "s/$result_name//g")" 
+		result_correct="$(cat "$1" | grep "$result_name" | sed -e 's/^[ \t]*//' | sed "s/$result_name//g")"
+		result_actual="$(echo "$2" | grep "$result_name" | sed -e 's/^[ \t]*//' | sed "s/$result_name//g")"
 		if [ "$result_correct" == "$result_actual" ]; then
-		echo "->$result_name correct!" | tee -a LOG.txt 
+		echo "->$result_name correct!" | tee -a LOG.txt
 		else
 			echo "==->ERROR: $result_name not correct!" | tee -a LOG.txt
 			echo "==->Actual: $result_correct" | tee -a LOG.txt
@@ -40,7 +40,7 @@ result_folder="Testrun-$today"
 mkdir "$result_folder"
 
 # Get scenario arguments and current commits
-octotiger_args="$(cat "$1" | grep 'octotiger_args' | sed 's/octotiger_args: //g')" 
+octotiger_args="$(cat "$1" | grep 'octotiger_args' | sed 's/octotiger_args: //g')"
 current_commit=`git rev-parse HEAD`
 
 # Log configuration
@@ -64,6 +64,64 @@ echo "#Number HPX threads,All off,m2m on,m2p on,p2p on,p2m on,All on,All on exce
 cp ../verification_and_scaling_test.sh used-script-copy.txt
 # Save scenario file for sanity check later on
 cp ../$1 $1
+
+# Save disassembly of performance critical functions for later analysis
+echo "" | tee -a LOG.txt
+echo "Getting assembly/source mix of kernels..." | tee -a LOG.txt
+mkdir kernel-mixed-assembly
+gdb ../$5 -batch \
+        -ex "disassemble /s octotiger::fmm::multipole_interactions::m2m_kernel::blocked_interaction_rho" \
+        -ex "quit" > kernel-mixed-assembly/m2m_rho.asm
+gdb ../$5 -batch \
+        -ex "disassemble /s octotiger::fmm::multipole_interactions::m2m_kernel::blocked_interaction_non_rho" \
+        -ex "quit" > kernel-mixed-assembly/m2m_non_rho.asm
+echo "Got multipole multipole kernels!" | tee -a LOG.txt
+gdb ../$5 -batch \
+        -ex "disassemble /s octotiger::fmm::multipole_interactions::m2p_kernel::blocked_interaction_rho" \
+        -ex "quit" > kernel-mixed-assembly/m2p_rho.asm
+gdb ../$5 -batch \
+        -ex "disassemble /s octotiger::fmm::multipole_interactions::m2p_kernel::blocked_interaction_non_rho" \
+        -ex "quit" > kernel-mixed-assembly/m2p_non_rho.asm
+echo "Got multipole monopole kernels!" | tee -a LOG.txt
+gdb ../$5 -batch \
+        -ex "disassemble /s octotiger::fmm::monopole_interactions::p2m_kernel::blocked_interaction_rho" \
+        -ex "quit" > kernel-mixed-assembly/p2m_rho.asm
+gdb ../$5 -batch \
+        -ex "disassemble /s octotiger::fmm::monopole_interactions::p2m_kernel::blocked_interaction_non_rho" \
+        -ex "quit" > kernel-mixed-assembly/p2m_non_rho.asm
+echo "Got monopole multipole kernels!" | tee -a LOG.txt
+gdb ../$5 -batch \
+        -ex "disassemble /s octotiger::fmm::monopole_interactions::p2p_kernel::blocked_interaction" \
+        -ex "quit" > kernel-mixed-assembly/p2p.asm
+echo "Got monopole monopole kernel!" | tee -a LOG.txt
+echo "" | tee -a LOG.txt
+echo "Getting  raw assembly of kernels..." | tee -a LOG.txt
+mkdir kernel-raw-assembly
+gdb ../$5 -batch \
+        -ex "disassemble octotiger::fmm::multipole_interactions::m2m_kernel::blocked_interaction_rho" \
+        -ex "quit" > kernel-raw-assembly/m2m_rho.asm
+gdb ../$5 -batch \
+        -ex "disassemble octotiger::fmm::multipole_interactions::m2m_kernel::blocked_interaction_non_rho" \
+        -ex "quit" > kernel-raw-assembly/m2m_non_rho.asm
+echo "Got multipole multipole kernels!" | tee -a LOG.txt
+gdb ../$5 -batch \
+        -ex "disassemble octotiger::fmm::multipole_interactions::m2p_kernel::blocked_interaction_rho" \
+        -ex "quit" > kernel-raw-assembly/m2p_rho.asm
+gdb ../$5 -batch \
+        -ex "disassemble octotiger::fmm::multipole_interactions::m2p_kernel::blocked_interaction_non_rho" \
+        -ex "quit" > kernel-raw-assembly/m2p_non_rho.asm
+echo "Got multipole monopole kernels!" | tee -a LOG.txt
+gdb ../$5 -batch \
+        -ex "disassemble octotiger::fmm::monopole_interactions::p2m_kernel::blocked_interaction_rho" \
+        -ex "quit" > kernel-raw-assembly/p2m_rho.asm
+gdb ../$5 -batch \
+        -ex "disassemble octotiger::fmm::monopole_interactions::p2m_kernel::blocked_interaction_non_rho" \
+        -ex "quit" > kernel-raw-assembly/p2m_non_rho.asm
+echo "Got monopole multipole kernels!" | tee -a LOG.txt
+gdb ../$5 -batch \
+        -ex "disassemble octotiger::fmm::monopole_interactions::p2p_kernel::blocked_interaction" \
+        -ex "quit" > kernel-raw-assembly/p1p.asm
+echo "Got monopole monopole kernel!" | tee -a LOG.txt
 
 # Need to use declare, otherwise bash assumes it is a string (untyped)
 declare -i errorcount=0
@@ -93,8 +151,8 @@ for i in `seq $2 $3 $4`; do
 	output7="$(../$5 -Ihpx.stacks.use_guard_pages=0 -t$i $octotiger_args p2p_kernel_type=soa_cpu p2m_kernel_type=old m2m_kernel_type=soa_cpu m2p_kernel_type=soa_cpu)"
 	check_results $1 "$output7"
 	# Clean up results
-	clean_output_computational="$i,$(echo "$output1" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output2" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output3" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output4" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output5" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output6" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output7" | grep 'Computation' | sed 's/Computation: //g')" 
-	clean_output_total="$i,$(echo "$output1" | grep 'Total' | sed 's/Total: //g'),$(echo "$output2" | grep 'Total' | sed 's/Total: //g'),$(echo "$output3" | grep 'Total' | sed 's/Total: //g'),$(echo "$output4" | grep 'Total' | sed 's/Total: //g'),$(echo "$output5" | grep 'Total' | sed 's/Total: //g'),$(echo "$output6" | grep 'Total' | sed 's/Total: //g'),$(echo "$output7" | grep 'Total' | sed 's/Total: //g')" 
+	clean_output_computational="$i,$(echo "$output1" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output2" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output3" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output4" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output5" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output6" | grep 'Computation' | sed 's/Computation: //g'),$(echo "$output7" | grep 'Computation' | sed 's/Computation: //g')"
+	clean_output_total="$i,$(echo "$output1" | grep 'Total' | sed 's/Total: //g'),$(echo "$output2" | grep 'Total' | sed 's/Total: //g'),$(echo "$output3" | grep 'Total' | sed 's/Total: //g'),$(echo "$output4" | grep 'Total' | sed 's/Total: //g'),$(echo "$output5" | grep 'Total' | sed 's/Total: //g'),$(echo "$output6" | grep 'Total' | sed 's/Total: //g'),$(echo "$output7" | grep 'Total' | sed 's/Total: //g')"
 	# Print and save to files >> for appending
 	echo "$clean_output_computational" >> "computation_time_results.txt"
 	echo "$clean_output_computational" | tee -a LOG.txt
