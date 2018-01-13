STEllAR-GROUP/octotiger

commit 4a156c96cc1b4507069bb6b88d2388ee2ca013bd

Reduce allocations and writes in multipole kernels

src/multipole_interactions/m2p_kernel.cpp
@@ -16,20 +16,16 @@ namespace octotiger {
 namespace fmm {
     namespace multipole_interactions {
 
-        m2p_kernel::m2p_kernel(std::vector<bool>& neighbor_empty, gsolve_type type, real dx,
-            std::array<real, NDIM> xbase)
+        m2p_kernel::m2p_kernel(std::vector<bool>& neighbor_empty)
           : neighbor_empty(neighbor_empty)
-          , type(type)
           , theta_rec_squared(sqr(1.0 / opts.theta))
-          , dX(dx)
-          , xBase(xbase)
         // , theta_rec_squared_scalar(sqr(1.0 / opts.theta))
         {
             for (size_t i = 0; i < m2m_int_vector::size(); i++) {
                 offset_vector[i] = i;
             }
-            vectors_check_empty();
             // calculate_coarse_indices();
+            vector_is_empty = std::vector<bool>(PADDED_STRIDE * PADDED_STRIDE * PADDED_STRIDE);
         }
 
         void m2p_kernel::apply_stencil(std::vector<real>& mons,
@@ -40,7 +36,9 @@ namespace fmm {
                 potential_expansions_SoA,
             struct_of_array_data<space_vector, real, 3, ENTRIES, SOA_PADDING>&
                 angular_corrections_SoA,
-            std::vector<multiindex<>>& stencil, std::vector<bool>& interact) {
+            std::vector<multiindex<>>& stencil, gsolve_type type, real dX,
+            std::array<real, NDIM>& xbase) {
+            vectors_check_empty();
             // for(auto i = 0; i < local_expansions.size(); i++)
             //   std::cout << local_expansions[i] << " ";
             // for (multiindex<>& stencil_element : stencil) {
@@ -83,14 +81,14 @@ namespace fmm {
                                     center_of_masses_SoA, potential_expansions_SoA,
                                     angular_corrections_SoA, cell_index, cell_flat_index,
                                     cell_index_coarse, cell_index_unpadded,
-                                    cell_flat_index_unpadded, stencil, outer_stencil_index,
-                                    interact);
+                                    cell_flat_index_unpadded, stencil, outer_stencil_index, dX,
+                                    xbase);
                             } else {
                                 this->blocked_interaction_non_rho(mons, center_of_masses_SoA,
                                     potential_expansions_SoA, angular_corrections_SoA, cell_index,
                                     cell_flat_index, cell_index_coarse, cell_index_unpadded,
-                                    cell_flat_index_unpadded, stencil, outer_stencil_index,
-                                    interact);
+                                    cell_flat_index_unpadded, stencil, outer_stencil_index, dX,
+                                    xbase);
                             }
                         }
                     }
@@ -99,7 +97,6 @@ namespace fmm {
         }
 
         void m2p_kernel::vectors_check_empty() {
-            vector_is_empty = std::vector<bool>(PADDED_STRIDE * PADDED_STRIDE * PADDED_STRIDE);
             for (size_t i0 = 0; i0 < PADDED_STRIDE; i0 += 1) {
                 for (size_t i1 = 0; i1 < PADDED_STRIDE; i1 += 1) {
                     for (size_t i2 = 0; i2 < PADDED_STRIDE; i2 += 1) {
@@ -127,6 +124,8 @@ namespace fmm {
                         } else {
                             vector_is_empty[cell_flat_index] = false;
                         }
+                        if (i0 >= 8 && i0 < 16 && i1 >= 8 && i1 < 16 && i2 >= 8 && i2 < 16)
+                            vector_is_empty[cell_flat_index] = true;
                     }
                 }
             }

src/multipole_interactions/m2m_kernel.cpp
@@ -25,8 +25,7 @@ namespace fmm {
             // potential_expansions_SoA,
             // struct_of_array_data<space_vector, real, 3, ENTRIES, SOA_PADDING>&
             // angular_corrections_SoA,
-            std::vector<bool>& neighbor_empty,
-            gsolve_type type)
+            std::vector<bool>& neighbor_empty)
           :    // local_expansions(local_expansions),
           // local_expansions_SoA(local_expansions_SoA)
           // , center_of_masses(center_of_masses),
@@ -36,14 +35,13 @@ namespace fmm {
           // , angular_corrections(angular_corrections)
           // , angular_corrections_SoA(angular_corrections_SoA),
           neighbor_empty(neighbor_empty)
-          , type(type)
           , theta_rec_squared(sqr(1.0 / opts.theta))
         // , theta_rec_squared_scalar(sqr(1.0 / opts.theta))
         {
             for (size_t i = 0; i < m2m_int_vector::size(); i++) {
                 offset_vector[i] = i;
             }
-            vectors_check_empty();
+            vector_is_empty = std::vector<bool>(PADDED_STRIDE * PADDED_STRIDE * PADDED_STRIDE);
             // calculate_coarse_indices();
         }
 
@@ -54,7 +52,8 @@ namespace fmm {
                 potential_expansions_SoA,
             struct_of_array_data<space_vector, real, 3, ENTRIES, SOA_PADDING>&
                 angular_corrections_SoA,
-            std::vector<multiindex<>>& stencil) {
+            std::vector<multiindex<>>& stencil, gsolve_type type) {
+            vectors_check_empty();
             // for (multiindex<>& stencil_element : stencil) {
             for (size_t outer_stencil_index = 0; outer_stencil_index < stencil.size();
                  outer_stencil_index += STENCIL_BLOCKING) {
@@ -121,7 +120,6 @@ namespace fmm {
         // }
 
         void m2m_kernel::vectors_check_empty() {
-            vector_is_empty = std::vector<bool>(PADDED_STRIDE * PADDED_STRIDE * PADDED_STRIDE);
             for (size_t i0 = 0; i0 < PADDED_STRIDE; i0 += 1) {
                 for (size_t i1 = 0; i1 < PADDED_STRIDE; i1 += 1) {
                     for (size_t i2 = 0; i2 < PADDED_STRIDE; i2 += 1) {

src/multipole_interactions/m2m_kernel.hpp
@@ -44,8 +44,6 @@ namespace fmm {
             // std::vector<int32_t> coarse_y;
             // std::vector<int32_t> coarse_z;
 
-            gsolve_type type;
-
             const m2m_vector theta_rec_squared;
             m2m_int_vector offset_vector;
 
@@ -161,7 +159,7 @@ namespace fmm {
                 //     potential_expansions_SoA,
                 // struct_of_array_data<space_vector, real, 3, ENTRIES, SOA_PADDING>&
                 //     angular_corrections_SoA,
-                std::vector<bool>& neighbor_empty, gsolve_type type);
+                std::vector<bool>& neighbor_empty);
 
             m2m_kernel(m2m_kernel& other) = delete;
 
@@ -177,7 +175,7 @@ namespace fmm {
                     potential_expansions_SoA,
                 struct_of_array_data<space_vector, real, 3, ENTRIES, SOA_PADDING>&
                     angular_corrections_SoA,
-                std::vector<multiindex<>>& stencil);
+                               std::vector<multiindex<>>& stencil, gsolve_type type);
         };
 
     }    // namespace multipole_interactions

src/multipole_interactions/m2m_kernel_blocked_interaction.cpp
@@ -71,6 +71,7 @@ namespace fmm {
             tmp_corrections[2] = angular_corrections_SoA.value<2>(cell_flat_index_unpadded);
             // struct_of_array_iterator<space_vector, real, 3> X(center_of_masses_SoA,
             // cell_flat_index);
+            bool changed_data = false;
             for (size_t inner_stencil_index = 0; inner_stencil_index < STENCIL_BLOCKING &&
                  outer_stencil_index + inner_stencil_index < stencil.size();
                  inner_stencil_index += 1) {
@@ -107,6 +108,7 @@ namespace fmm {
                 if (Vc::none_of(mask)) {
                     continue;
                 }
+                changed_data = true;
 
                 // struct_of_array_iterator<space_vector, real, 3> Y(
                 //     center_of_masses_SoA, interaction_partner_flat_index);
@@ -448,56 +450,68 @@ namespace fmm {
                 Vc::where(mask, tmp_corrections[2]) =
                     tmp_corrections[2] + current_angular_correction[2];
             }
-            tmpstore[0].memstore(potential_expansions_SoA.pointer<0>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[1].memstore(potential_expansions_SoA.pointer<1>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[2].memstore(potential_expansions_SoA.pointer<2>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[3].memstore(potential_expansions_SoA.pointer<3>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[4].memstore(potential_expansions_SoA.pointer<4>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[5].memstore(potential_expansions_SoA.pointer<5>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[6].memstore(potential_expansions_SoA.pointer<6>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[7].memstore(potential_expansions_SoA.pointer<7>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[8].memstore(potential_expansions_SoA.pointer<8>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[9].memstore(potential_expansions_SoA.pointer<9>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[10].memstore(potential_expansions_SoA.pointer<10>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[11].memstore(potential_expansions_SoA.pointer<11>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[12].memstore(potential_expansions_SoA.pointer<12>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[13].memstore(potential_expansions_SoA.pointer<13>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[14].memstore(potential_expansions_SoA.pointer<14>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[15].memstore(potential_expansions_SoA.pointer<15>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[16].memstore(potential_expansions_SoA.pointer<16>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[17].memstore(potential_expansions_SoA.pointer<17>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[18].memstore(potential_expansions_SoA.pointer<18>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[19].memstore(potential_expansions_SoA.pointer<19>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
+            if (changed_data) {
+                tmpstore[0].memstore(potential_expansions_SoA.pointer<0>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[1].memstore(potential_expansions_SoA.pointer<1>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[2].memstore(potential_expansions_SoA.pointer<2>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[3].memstore(potential_expansions_SoA.pointer<3>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[4].memstore(potential_expansions_SoA.pointer<4>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[5].memstore(potential_expansions_SoA.pointer<5>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[6].memstore(potential_expansions_SoA.pointer<6>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[7].memstore(potential_expansions_SoA.pointer<7>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[8].memstore(potential_expansions_SoA.pointer<8>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[9].memstore(potential_expansions_SoA.pointer<9>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[10].memstore(
+                    potential_expansions_SoA.pointer<10>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[11].memstore(
+                    potential_expansions_SoA.pointer<11>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[12].memstore(
+                    potential_expansions_SoA.pointer<12>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[13].memstore(
+                    potential_expansions_SoA.pointer<13>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[14].memstore(
+                    potential_expansions_SoA.pointer<14>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[15].memstore(
+                    potential_expansions_SoA.pointer<15>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[16].memstore(
+                    potential_expansions_SoA.pointer<16>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[17].memstore(
+                    potential_expansions_SoA.pointer<17>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[18].memstore(
+                    potential_expansions_SoA.pointer<18>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[19].memstore(
+                    potential_expansions_SoA.pointer<19>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
 
-            tmp_corrections[0].memstore(
-                angular_corrections_SoA.pointer<0>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmp_corrections[1].memstore(
-                angular_corrections_SoA.pointer<1>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmp_corrections[2].memstore(
-                angular_corrections_SoA.pointer<2>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
+                tmp_corrections[0].memstore(
+                    angular_corrections_SoA.pointer<0>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmp_corrections[1].memstore(
+                    angular_corrections_SoA.pointer<1>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmp_corrections[2].memstore(
+                    angular_corrections_SoA.pointer<2>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+            }
         }
 
         void m2m_kernel::blocked_interaction_non_rho(
@@ -538,6 +552,7 @@ namespace fmm {
             tmpstore[19] = potential_expansions_SoA.value<19>(cell_flat_index_unpadded);
             // struct_of_array_iterator<space_vector, real, 3> X(center_of_masses_SoA,
             // cell_flat_index);
+            bool changed_data = false;
             for (size_t inner_stencil_index = 0; inner_stencil_index < STENCIL_BLOCKING &&
                  outer_stencil_index + inner_stencil_index < stencil.size();
                  inner_stencil_index += 1) {
@@ -574,6 +589,7 @@ namespace fmm {
                 if (Vc::none_of(mask)) {
                     continue;
                 }
+                changed_data = true;
 
                 // struct_of_array_iterator<space_vector, real, 3> Y(
                 //     center_of_masses_SoA, interaction_partner_flat_index);
@@ -745,46 +761,58 @@ namespace fmm {
                 Vc::where(mask, tmpstore[18]) = tmpstore[18] + m_partner[0] * D_lower[18];
                 Vc::where(mask, tmpstore[19]) = tmpstore[19] + m_partner[0] * D_lower[19];
             }
-            tmpstore[0].memstore(potential_expansions_SoA.pointer<0>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[1].memstore(potential_expansions_SoA.pointer<1>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[2].memstore(potential_expansions_SoA.pointer<2>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[3].memstore(potential_expansions_SoA.pointer<3>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[4].memstore(potential_expansions_SoA.pointer<4>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[5].memstore(potential_expansions_SoA.pointer<5>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[6].memstore(potential_expansions_SoA.pointer<6>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[7].memstore(potential_expansions_SoA.pointer<7>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[8].memstore(potential_expansions_SoA.pointer<8>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[9].memstore(potential_expansions_SoA.pointer<9>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[10].memstore(potential_expansions_SoA.pointer<10>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[11].memstore(potential_expansions_SoA.pointer<11>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[12].memstore(potential_expansions_SoA.pointer<12>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[13].memstore(potential_expansions_SoA.pointer<13>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[14].memstore(potential_expansions_SoA.pointer<14>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[15].memstore(potential_expansions_SoA.pointer<15>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[16].memstore(potential_expansions_SoA.pointer<16>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[17].memstore(potential_expansions_SoA.pointer<17>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[18].memstore(potential_expansions_SoA.pointer<18>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
-            tmpstore[19].memstore(potential_expansions_SoA.pointer<19>(cell_flat_index_unpadded),
-                Vc::flags::element_aligned);
+            if (changed_data) {
+                tmpstore[0].memstore(potential_expansions_SoA.pointer<0>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[1].memstore(potential_expansions_SoA.pointer<1>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[2].memstore(potential_expansions_SoA.pointer<2>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[3].memstore(potential_expansions_SoA.pointer<3>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[4].memstore(potential_expansions_SoA.pointer<4>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[5].memstore(potential_expansions_SoA.pointer<5>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[6].memstore(potential_expansions_SoA.pointer<6>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[7].memstore(potential_expansions_SoA.pointer<7>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[8].memstore(potential_expansions_SoA.pointer<8>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[9].memstore(potential_expansions_SoA.pointer<9>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[10].memstore(
+                    potential_expansions_SoA.pointer<10>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[11].memstore(
+                    potential_expansions_SoA.pointer<11>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[12].memstore(
+                    potential_expansions_SoA.pointer<12>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[13].memstore(
+                    potential_expansions_SoA.pointer<13>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[14].memstore(
+                    potential_expansions_SoA.pointer<14>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[15].memstore(
+                    potential_expansions_SoA.pointer<15>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[16].memstore(
+                    potential_expansions_SoA.pointer<16>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[17].memstore(
+                    potential_expansions_SoA.pointer<17>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[18].memstore(
+                    potential_expansions_SoA.pointer<18>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+                tmpstore[19].memstore(
+                    potential_expansions_SoA.pointer<19>(cell_flat_index_unpadded),
+                    Vc::flags::element_aligned);
+            }
         }
     }    // namespace multipole_interactions
 }    // namespace fmm

commit 984603e264cfa5ea64b11735fef2a31473556775

Fixed numerical issues
This should fix the numerical issues that were introduced in
4a156c9 and 27e7d63.

src/monopole_interactions/p2m_kernel.cpp
@@ -23,7 +23,7 @@ namespace fmm {
             for (size_t i = 0; i < m2m_int_vector::size(); i++) {
                 offset_vector[i] = i;
             }
-            vector_is_empty = std::vector<bool>(PADDED_STRIDE * PADDED_STRIDE * PADDED_STRIDE);
+            vectors_check_empty();
         }
 
         void p2m_kernel::apply_stencil(
@@ -34,7 +34,6 @@ namespace fmm {
             struct_of_array_data<space_vector, real, 3, ENTRIES, SOA_PADDING>&
                 angular_corrections_SoA,
             std::vector<multiindex<>>& stencil, gsolve_type type) {
-            vectors_check_empty();
             // for(auto i = 0; i < local_expansions.size(); i++)
             //   std::cout << local_expansions[i] << " ";
             // for (multiindex<>& stencil_element : stencil) {
@@ -92,6 +91,7 @@ namespace fmm {
         }
 
         void p2m_kernel::vectors_check_empty() {
+            vector_is_empty = std::vector<bool>(PADDED_STRIDE * PADDED_STRIDE * PADDED_STRIDE);
             for (size_t i0 = 0; i0 < PADDED_STRIDE; i0 += 1) {
                 for (size_t i1 = 0; i1 < PADDED_STRIDE; i1 += 1) {
                     for (size_t i2 = 0; i2 < PADDED_STRIDE; i2 += 1) {
@@ -119,8 +119,8 @@ namespace fmm {
                         } else {
                             vector_is_empty[cell_flat_index] = false;
                         }
-                        if (i0 >= 8 && i0 < 16 && i1 >= 8 && i1 < 16 && i2 >= 8 && i2 < 16)
-                            vector_is_empty[cell_flat_index] = true;
+                        // if (i0 >= 8 && i0 < 16 && i1 >= 8 && i1 < 16 && i2 >= 8 && i2 < 16)
+                        //     vector_is_empty[cell_flat_index] = true;
                     }
                 }
             }

src/monopole_interactions/p2p_kernel.cpp
@@ -24,15 +24,14 @@ namespace fmm {
             for (size_t i = 0; i < m2m_int_vector::size(); i++) {
                 offset_vector[i] = i;
             }
-            vector_is_empty = std::vector<bool>(PADDED_STRIDE * PADDED_STRIDE * PADDED_STRIDE);
             // calculate_coarse_indices();
+            vectors_check_empty();
         }
 
         void p2p_kernel::apply_stencil(std::vector<real>& local_expansions,
             struct_of_array_data<expansion, real, 20, ENTRIES, SOA_PADDING>&
                 potential_expansions_SoA,
             std::vector<multiindex<>>& stencil, std::vector<std::array<real, 4>>& four, real dx) {
-            vectors_check_empty();
             // for(auto i = 0; i < local_expansions.size(); i++)
             //   std::cout << local_expansions[i] << " ";
             // for (multiindex<>& stencil_element : stencil) {
@@ -77,6 +76,7 @@ namespace fmm {
         }
 
         void p2p_kernel::vectors_check_empty() {
+            vector_is_empty = std::vector<bool>(PADDED_STRIDE * PADDED_STRIDE * PADDED_STRIDE);
             for (size_t i0 = 0; i0 < PADDED_STRIDE; i0 += 1) {
                 for (size_t i1 = 0; i1 < PADDED_STRIDE; i1 += 1) {
                     for (size_t i2 = 0; i2 < PADDED_STRIDE; i2 += 1) {

src/multipole_interactions/m2p_kernel.cpp
@@ -24,8 +24,8 @@ namespace fmm {
             for (size_t i = 0; i < m2m_int_vector::size(); i++) {
                 offset_vector[i] = i;
             }
+            vectors_check_empty();
             // calculate_coarse_indices();
-            vector_is_empty = std::vector<bool>(PADDED_STRIDE * PADDED_STRIDE * PADDED_STRIDE);
         }
 
         void m2p_kernel::apply_stencil(std::vector<real>& mons,
@@ -38,7 +38,6 @@ namespace fmm {
                 angular_corrections_SoA,
             std::vector<multiindex<>>& stencil, gsolve_type type, real dX,
             std::array<real, NDIM>& xbase) {
-            vectors_check_empty();
             // for(auto i = 0; i < local_expansions.size(); i++)
             //   std::cout << local_expansions[i] << " ";
             // for (multiindex<>& stencil_element : stencil) {
@@ -97,6 +96,7 @@ namespace fmm {
         }
 
         void m2p_kernel::vectors_check_empty() {
+            vector_is_empty = std::vector<bool>(PADDED_STRIDE * PADDED_STRIDE * PADDED_STRIDE);
             for (size_t i0 = 0; i0 < PADDED_STRIDE; i0 += 1) {
                 for (size_t i1 = 0; i1 < PADDED_STRIDE; i1 += 1) {
                     for (size_t i2 = 0; i2 < PADDED_STRIDE; i2 += 1) {
@@ -124,8 +124,8 @@ namespace fmm {
                         } else {
                             vector_is_empty[cell_flat_index] = false;
                         }
-                        if (i0 >= 8 && i0 < 16 && i1 >= 8 && i1 < 16 && i2 >= 8 && i2 < 16)
-                            vector_is_empty[cell_flat_index] = true;
+                        // if (i0 >= 8 && i0 < 16 && i1 >= 8 && i1 < 16 && i2 >= 8 && i2 < 16)
+                        //     vector_is_empty[cell_flat_index] = true;
                     }
                 }
             }