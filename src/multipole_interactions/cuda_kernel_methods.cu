cb8875b98b168fe878dfb0ba913a8e90c00078a8
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2018-03-14 14:39:29 -0500

    Fixed cuda kernel array indices

--- src/multipole_interactions/cuda_kernel_methods.cu
+++ src/multipole_interactions/cuda_kernel_methods.cu
@@ -19,6 +19,7 @@ namespace fmm {
     }
 
     namespace multipole_interactions {
+
         constexpr size_t component_length = ENTRIES + SOA_PADDING;
         constexpr size_t component_length_unpadded = INNER_CELLS + SOA_PADDING;
         __global__ void cuda_multipole_interactions_kernel(
@@ -28,9 +29,9 @@ namespace fmm {
             double (&potential_expansions)[NUMBER_POT_EXPANSIONS],
             double (&angular_corrections)[NUMBER_ANG_CORRECTIONS],
             octotiger::fmm::multiindex<> (&stencil)[STENCIL_SIZE],
-            bool (&stencil_phases)[STENCIL_SIZE], double (&factor_half)[20],
+            double (&stencil_phases)[STENCIL_SIZE], double (&factor_half)[20],
             double (&factor_sixth)[20], double theta) {
-            printf("yay %f", theta);
+            // printf("yay %f", theta);
 
             // Set cell indices
             const octotiger::fmm::multiindex<> cell_index(threadIdx.x + INNER_CELLS_PADDING_DEPTH,
@@ -72,18 +73,20 @@ namespace fmm {
             // Create and set result arrays
             double tmpstore[20];
             double tmp_corrections[3];
-            for (size_t i = 0; i < 20; ++i)
-                tmpstore[i] = 0.0;
-            for (size_t i = 0; i < 3; ++i)
-                tmp_corrections[i] = 0.0;
+            // for (size_t i = 0; i < 20; ++i)
+            //     tmpstore[i] = 0.0;
+            // for (size_t i = 0; i < 3; ++i)
+            //     tmp_corrections[i] = 0.0;
             // Required for mask
             const double theta_rec_squared = sqr(1.0 / theta);
+            double m_partner[20];
+            double Y[NDIM];
 
             // calculate interactions between this cell and each stencil element
             for (size_t stencil_index = 0; stencil_index < STENCIL_SIZE; stencil_index++) {
                 // Get phase indicator (indicates whether multipole multipole interactions still
                 // needs to be done)
-                const double mask_phase_one = static_cast<double>(stencil_phases[stencil_index]);
+                const double mask_phase_one = stencil_phases[stencil_index];
 
                 // Get interaction partner indices
                 const multiindex<>& stencil_element = stencil[stencil_index];
@@ -100,17 +103,16 @@ namespace fmm {
                 double mask = mask_b ? 1.0 : 0.0;
 
                 // Load data of interaction partner
-                double Y[NDIM];
                 Y[0] = center_of_masses[partner_flat_index];
                 Y[1] = center_of_masses[1 * component_length + partner_flat_index];
                 Y[2] = center_of_masses[2 * component_length + partner_flat_index];
-                double m_partner[20];
                 m_partner[0] = local_monopoles[partner_flat_index] * mask;
                 mask = mask * mask_phase_one;    // do not load multipoles outside the inner stencil
                 m_partner[0] += multipoles[partner_flat_index] * mask;
                 m_partner[1] = multipoles[1 * component_length + partner_flat_index] * mask;
                 m_partner[2] = multipoles[2 * component_length + partner_flat_index] * mask;
                 m_partner[3] = multipoles[3 * component_length + partner_flat_index] * mask;
+                // printf("%d ", m_partner[3]);
                 m_partner[4] = multipoles[4 * component_length + partner_flat_index] * mask;
                 m_partner[5] = multipoles[5 * component_length + partner_flat_index] * mask;
                 m_partner[6] = multipoles[6 * component_length + partner_flat_index] * mask;
@@ -127,37 +129,80 @@ namespace fmm {
                 m_partner[17] = multipoles[17 * component_length + partner_flat_index] * mask;
                 m_partner[18] = multipoles[18 * component_length + partner_flat_index] * mask;
                 m_partner[19] = multipoles[19 * component_length + partner_flat_index] * mask;
+                // printf("%f ", multipoles[0 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[1 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[2 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[3 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[4 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[5 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[6 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[7 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[8 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[9 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[10 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[11 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[12 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[13 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[14 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[15 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[16 * component_length + partner_flat_index]);
+                // printf("%f ", multipoles[17 * component_length + partner_flat_index]);
+                // printf("\n");
+
+
+
 
                 // Do the actual calculations
                 compute_kernel_rho<double>(
-                    X, Y, m_partner, tmpstore, tmp_corrections, m_cell, factor_half,
-                    factor_sixth);
+                    X, Y, m_partner, tmpstore, tmp_corrections, m_cell, factor_half, factor_sixth);
             }
             // Store results in output arrays
             potential_expansions[cell_flat_index_unpadded] = tmpstore[0];
-            potential_expansions[1 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[1];
-            potential_expansions[2 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[2];
-            potential_expansions[3 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[3];
-            potential_expansions[4 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[4];
-            potential_expansions[5 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[5];
-            potential_expansions[6 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[6];
-            potential_expansions[7 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[7];
-            potential_expansions[8 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[8];
-            potential_expansions[9 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[9];
-            potential_expansions[10 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[10];
-            potential_expansions[11 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[11];
-            potential_expansions[12 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[12];
-            potential_expansions[13 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[13];
-            potential_expansions[14 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[14];
-            potential_expansions[15 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[15];
-            potential_expansions[16 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[16];
-            potential_expansions[17 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[17];
-            potential_expansions[18 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[18];
-            potential_expansions[19 * component_length_unpadded * cell_flat_index_unpadded] = tmpstore[19];
+            potential_expansions[1 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[1];
+            potential_expansions[2 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[2];
+            potential_expansions[3 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[3];
+            potential_expansions[4 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[4];
+            // printf("%d ", tmpstore[3]);
+            potential_expansions[5 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[5];
+            potential_expansions[6 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[6];
+            potential_expansions[7 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[7];
+            potential_expansions[8 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[8];
+            potential_expansions[9 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[9];
+            potential_expansions[10 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[10];
+            potential_expansions[11 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[11];
+            potential_expansions[12 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[12];
+            potential_expansions[13 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[13];
+            potential_expansions[14 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[14];
+            potential_expansions[15 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[15];
+            potential_expansions[16 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[16];
+            potential_expansions[17 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[17];
+            potential_expansions[18 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[18];
+            potential_expansions[19 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmpstore[19];
 
             angular_corrections[cell_flat_index_unpadded] = tmp_corrections[0];
-            angular_corrections[1 * component_length_unpadded * cell_flat_index_unpadded] = tmp_corrections[1];
-            angular_corrections[2 * component_length_unpadded * cell_flat_index_unpadded] = tmp_corrections[2];
+            angular_corrections[1 * component_length_unpadded + cell_flat_index_unpadded] =
+            tmp_corrections[1];
+            angular_corrections[2 * component_length_unpadded + cell_flat_index_unpadded] =
+            tmp_corrections[2];
         }
     }    // namespace multipole_interactions
 }    // namespace fmm
