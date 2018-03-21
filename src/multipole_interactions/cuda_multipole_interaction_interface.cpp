b957f58e25147476e8f4bd77be90dcd860253e04
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2018-03-20 22:00:29 -0500

    Moved kernel_scheduler into its own file

--- src/multipole_interactions/cuda_multipole_interaction_interface.cpp
+++ src/multipole_interactions/cuda_multipole_interaction_interface.cpp
@@ -1,5 +1,6 @@
 #ifdef OCTOTIGER_CUDA_ENABLED
 #include "cuda_multipole_interaction_interface.hpp"
+#include "multipole_cuda_kernel.hpp"
 #include "calculate_stencil.hpp"
 #include "options.hpp"
 
@@ -7,124 +8,7 @@ extern options opts;
 namespace octotiger {
 namespace fmm {
     namespace multipole_interactions {
-        // thread_local util::cuda_helper cuda_multipole_interaction_interface::gpu_interface;
         thread_local kernel_scheduler cuda_multipole_interaction_interface::scheduler;
-        // Define sizes of buffers
-        constexpr size_t local_monopoles_size = NUMBER_LOCAL_MONOPOLE_VALUES * sizeof(real);
-        constexpr size_t local_expansions_size = NUMBER_LOCAL_EXPANSION_VALUES * sizeof(real);
-        constexpr size_t center_of_masses_size = NUMBER_MASS_VALUES * sizeof(real);
-        constexpr size_t potential_expansions_size = NUMBER_POT_EXPANSIONS * sizeof(real);
-        constexpr size_t angular_corrections_size = NUMBER_ANG_CORRECTIONS * sizeof(real);
-        constexpr size_t stencil_size = STENCIL_SIZE * sizeof(octotiger::fmm::multiindex<>);
-        constexpr size_t indicator_size = STENCIL_SIZE * sizeof(real);
-
-        kernel_scheduler::kernel_scheduler(void)
-          : number_cuda_streams_managed(opts.cuda_streams_per_thread)
-          , slots_per_cuda_stream(1)
-          , number_slots(number_cuda_streams_managed * slots_per_cuda_stream) {
-            stream_interfaces = std::vector<util::cuda_helper>(number_cuda_streams_managed);
-
-            local_expansions_slots =
-                std::vector<struct_of_array_data<expansion, real, 20, ENTRIES, SOA_PADDING,
-                                std::vector<real, cuda_pinned_allocator<real>>>>(
-                    number_slots);
-            center_of_masses_slots =
-                std::vector<struct_of_array_data<space_vector, real, 3, ENTRIES, SOA_PADDING,
-                                std::vector<real, cuda_pinned_allocator<real>>>>(
-                    number_slots);
-            local_monopole_slots = std::vector<std::vector<real, cuda_pinned_allocator<real>>>(number_slots);
-            for (std::vector<real, cuda_pinned_allocator<real>>& mons : local_monopole_slots) {
-                mons = std::vector<real, cuda_pinned_allocator<real>>(ENTRIES);
-            }
-
-            kernel_device_enviroments = std::vector<kernel_device_enviroment>(number_slots);
-            size_t cur_interface = 0;
-            size_t cur_slot = 0;
-
-            // Create necessary data
-            const two_phase_stencil stencil = calculate_stencil();
-            std::unique_ptr<real[]> indicator = std::make_unique<real[]>(STENCIL_SIZE);
-            for (auto i = 0; i < STENCIL_SIZE; ++i) {
-                if (stencil.stencil_phase_indicator[i])
-                    indicator[i] = 1.0;
-                else
-                    indicator[i] = 0.0;
-            }
-
-            for (kernel_device_enviroment& env : kernel_device_enviroments) {
-                // Allocate memory on device
-                util::cuda_helper::cuda_error(
-                    cudaMalloc((void**) &(env.device_local_monopoles), local_monopoles_size));
-                util::cuda_helper::cuda_error(
-                    cudaMalloc((void**) &(env.device_local_expansions), local_expansions_size));
-                util::cuda_helper::cuda_error(
-                    cudaMalloc((void**) &(env.device_center_of_masses), center_of_masses_size));
-                util::cuda_helper::cuda_error(cudaMalloc(
-                    (void**) &(env.device_potential_expansions), potential_expansions_size));
-                util::cuda_helper::cuda_error(cudaMalloc(
-                    (void**) &(env.device_angular_corrections), angular_corrections_size));
-                util::cuda_helper::cuda_error(
-                    cudaMalloc((void**) &(env.device_stencil), stencil_size));
-                util::cuda_helper::cuda_error(
-                    cudaMalloc((void**) &(env.device_phase_indicator), indicator_size));
-
-                // Move data
-                stream_interfaces[cur_interface].copy_async(env.device_stencil,
-                    stencil.stencil_elements.data(), stencil_size, cudaMemcpyHostToDevice);
-                stream_interfaces[cur_interface].copy_async(env.device_phase_indicator,
-                    indicator.get(), indicator_size, cudaMemcpyHostToDevice);
-
-                // Change stream interface if necessary
-                cur_slot++;
-                if (cur_slot >= slots_per_cuda_stream) {
-                    util::cuda_helper::cuda_error(cudaThreadSynchronize());
-                    cur_slot = 0;
-                    cur_interface++;
-                }
-            }
-            util::cuda_helper::cuda_error(cudaThreadSynchronize());
-            // for (auto i = 0; i < number_slots; ++i) {
-            //     slot_guards[i] = false;
-            // }
-        }
-
-        kernel_scheduler::~kernel_scheduler(void) {
-            // Deallocate device buffers
-            for (kernel_device_enviroment& env : kernel_device_enviroments) {
-                util::cuda_helper::cuda_error(cudaFree((void*) (env.device_local_monopoles)));
-                util::cuda_helper::cuda_error(cudaFree((void*) (env.device_local_expansions)));
-                util::cuda_helper::cuda_error(cudaFree((void*) (env.device_center_of_masses)));
-                util::cuda_helper::cuda_error(cudaFree((void*) (env.device_potential_expansions)));
-                util::cuda_helper::cuda_error(cudaFree((void*) (env.device_angular_corrections)));
-                util::cuda_helper::cuda_error(cudaFree((void*) (env.device_stencil)));
-                util::cuda_helper::cuda_error(cudaFree((void*) (env.device_phase_indicator)));
-            }
-        }
-
-        int kernel_scheduler::get_launch_slot(void) {
-            for (size_t slot_id = 0; slot_id < number_cuda_streams_managed; ++slot_id) {
-                const cudaError_t response = stream_interfaces[slot_id].pass_through(
-                    [](cudaStream_t& stream) -> cudaError_t { return cudaStreamQuery(stream); });
-                if (response == cudaSuccess)    // slot is free
-                    return slot_id;
-            }
-            // No slots available
-            return -1;
-        }
-
-        kernel_staging_area kernel_scheduler::get_staging_area(size_t slot) {
-            return kernel_staging_area(local_monopole_slots[slot], local_expansions_slots[slot],
-                center_of_masses_slots[slot]);
-        }
-
-        kernel_device_enviroment& kernel_scheduler::get_device_enviroment(size_t slot) {
-            return kernel_device_enviroments[slot];
-        }
-
-        util::cuda_helper& kernel_scheduler::get_launch_interface(size_t slot) {
-            size_t interface = slot / slots_per_cuda_stream;
-            return stream_interfaces[slot];
-        }
 
         cuda_multipole_interaction_interface::cuda_multipole_interaction_interface(void)
           : multipole_interaction_interface()
--- src/multipole_interactions/cuda_multipole_interaction_interface.hpp
+++ src/multipole_interactions/cuda_multipole_interaction_interface.hpp
@@ -1,124 +1,25 @@
 #pragma once
-#include "multipole_interaction_interface.hpp"
 #ifdef OCTOTIGER_CUDA_ENABLED
 #include <functional>
 #include "../cuda_util/cuda_helper.hpp"
-#include "multipole_cuda_kernel.hpp"
+#include "../cuda_util/cuda_scheduler.hpp"
+#include "multipole_interaction_interface.hpp"
 
 namespace octotiger {
 namespace fmm {
     namespace multipole_interactions {
-        /// Custom allocator for host-side cuda vectors
-        template <class T>
-        struct cuda_pinned_allocator
-        {
-            typedef T value_type;
-            cuda_pinned_allocator() noexcept {}
-            template <class U>
-            cuda_pinned_allocator(const cuda_pinned_allocator<U>&) noexcept {}
-            T* allocate(std::size_t n) {
-                T* data;
-                util::cuda_helper::cuda_error(cudaMallocHost((void**) &data, n * sizeof(T)));
-                return data;
-            }
-            void deallocate(T* p, std::size_t n) {
-                util::cuda_helper::cuda_error(cudaFreeHost(p));
-            }
-        };
-
-        template <class T, class U>
-        constexpr bool operator==(
-            const cuda_pinned_allocator<T>&, const cuda_pinned_allocator<U>&) noexcept {
-            return true;
-        }
-
-        template <class T, class U>
-        constexpr bool operator!=(
-            const cuda_pinned_allocator<T>&, const cuda_pinned_allocator<U>&) noexcept {
-            return false;
-        }
-
-        /// Contains references to all data needed for one FMM interaction kernel run
-        class kernel_staging_area
-        {
-        public:
-            kernel_staging_area(
-                std::vector<real, cuda_pinned_allocator<real>>& local_monopoles,
-                struct_of_array_data<expansion, real, 20, ENTRIES, SOA_PADDING,
-                    std::vector<real, cuda_pinned_allocator<real>>>& local_expansions_SoA,
-                struct_of_array_data<space_vector, real, 3, ENTRIES, SOA_PADDING,
-                    std::vector<real, cuda_pinned_allocator<real>>>& center_of_masses_SoA)
-              : local_monopoles(local_monopoles)
-              , local_expansions_SoA(local_expansions_SoA)
-              , center_of_masses_SoA(center_of_masses_SoA) {}
-            std::vector<real, cuda_pinned_allocator<real>>& local_monopoles;
-            struct_of_array_data<expansion, real, 20, ENTRIES, SOA_PADDING,
-                std::vector<real, cuda_pinned_allocator<real>>>& local_expansions_SoA;
-            struct_of_array_data<space_vector, real, 3, ENTRIES, SOA_PADDING,
-                std::vector<real, cuda_pinned_allocator<real>>>& center_of_masses_SoA;
-        };
-
-        /// Contains pointers to device buffers
-        struct kernel_device_enviroment
-        {
-            real* device_local_monopoles;
-            real* device_local_expansions;
-            real* device_center_of_masses;
-            real* device_potential_expansions;
-            real* device_angular_corrections;
-
-            octotiger::fmm::multiindex<>* device_stencil;
-            real* device_phase_indicator;
-        };
-
-        /// Scheduler which decides on what device to launch kernel and what memory to use
-        class kernel_scheduler
-        {
-        public:
-            /// Constructs number of streams indicated by the options
-            kernel_scheduler(void);
-            /// Deallocates cuda memory
-            ~kernel_scheduler(void);
-
-            /// Get a slot on any device to run a FMM kernel. Return -1 for CPU slot, else the slot
-            /// ID
-            int get_launch_slot(void);
-            /// Get references to SoA memory for a slot
-            inline kernel_staging_area get_staging_area(size_t slot);
-            /// Get references to SoA memory for a slot
-            inline kernel_device_enviroment& get_device_enviroment(size_t slot);
-            /// Get the cuda interface for a slot - throws exception if a CPU slot (-1)is given
-            inline util::cuda_helper& get_launch_interface(size_t slot);
-
-            kernel_scheduler(kernel_scheduler& other) = delete;
-            kernel_scheduler(const kernel_scheduler& other) = delete;
-            kernel_scheduler operator=(const kernel_scheduler& other) = delete;
-
-        private:
-            /// How many cuda streams does scheduler manage
-            const size_t number_cuda_streams_managed;
-            /// How many slots are there per stram - basically the queue length per stream
-            const size_t slots_per_cuda_stream;
-            /// How many slots are there
-            const size_t number_slots;
-
-            /// Contains number_cuda_streams_managed cuda interfaces
-            std::vector<util::cuda_helper> stream_interfaces;
-            std::vector<struct_of_array_data<expansion, real, 20, ENTRIES, SOA_PADDING,
-                std::vector<real, cuda_pinned_allocator<real>>>>
-                local_expansions_slots;
-            std::vector<struct_of_array_data<space_vector, real, 3, ENTRIES, SOA_PADDING,
-                std::vector<real, cuda_pinned_allocator<real>>>>
-                center_of_masses_slots;
-            std::vector<std::vector<real, cuda_pinned_allocator<real>>> local_monopole_slots;
-
-            std::vector<kernel_device_enviroment> kernel_device_enviroments;
-        };
-
+        /** Interface to calculate multipole FMM interactions on either a cuda device or on the
+         * cpu! It takes AoS data, transforms it into SoA data, moves it to the cuda device,
+         * launches cuda kernel on a slot given by the scheduler, gets results and stores them in
+         * the AoS arrays L and L_c. Load balancing between CPU and GPU is done by the scheduler (see
+         * ../cuda_util/cuda_scheduler.hpp). If the scheduler detects that the cuda device is
+         * busy it will use the normal CPU implementation of the interface as fallback!
+         */
         class cuda_multipole_interaction_interface : public multipole_interaction_interface
         {
         public:
             cuda_multipole_interaction_interface(void);
+            /// Takes AoS input, converts, launches kernel and writes AoS results back into L,L_c
             void compute_multipole_interactions(std::vector<real>& monopoles,
                 std::vector<multipole>& M_ptr,
                 std::vector<std::shared_ptr<std::vector<space_vector>>>& com_ptr,
@@ -127,15 +28,17 @@ namespace fmm {
                 std::array<real, NDIM> xbase);
 
         protected:
-            void queue_multipole_kernel(void) {}
-
-        protected:
+            /** Scheduler which controls cuda buffers, host-side pinned memory for the input and
+             * load-balancing
+             * between CPU/GPU */
             static thread_local kernel_scheduler scheduler;
             real theta;
 
+            /// Host-side pinned memory buffer for angular corrections results
             struct_of_array_data<space_vector, real, 3, INNER_CELLS, SOA_PADDING,
                 std::vector<real, cuda_pinned_allocator<real>>>
                 angular_corrections_SoA;
+            /// Host-side pinned memory buffer for potential expansions results
             struct_of_array_data<expansion, real, 20, INNER_CELLS, SOA_PADDING,
                 std::vector<real, cuda_pinned_allocator<real>>>
                 potential_expansions_SoA;
