c92ab32f95b5bc5cea627a9d72d470ffb51c16f3
Dominic Marcello <dmarce504@gmail.com>
2018-10-03 14:39:16 -0500

    Update build-octotiger.sh

--- circle_ci_scripts/build-octotiger.sh
+++ circle_ci_scripts/build-octotiger.sh
@@ -3,6 +3,6 @@ set -x
 
 mkdir build
 cd build
-cmake -DBOOST_ROOT=/home/circleci/boost_1_63_0_install -DOCTOTIGER_WITH_SILO=OFF -DCMAKE_PREFIX_PATH=/home/circleci/hpx_install -DCMAKE_BUILD_TYPE=release -DHPX_IGNORE_COMPILER_COMPATIBILITY=ON ../
+cmake -DCMAKE_CXX_FLAGS="-march=native" -DBOOST_ROOT=/home/circleci/boost_1_63_0_install -DOCTOTIGER_WITH_SILO=OFF -DCMAKE_PREFIX_PATH=/home/circleci/hpx_install -DCMAKE_BUILD_TYPE=release -DHPX_IGNORE_COMPILER_COMPATIBILITY=ON ../
 make -j2 VERBOSE=1
 cd ..
