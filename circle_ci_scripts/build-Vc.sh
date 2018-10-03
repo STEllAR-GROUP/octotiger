54fa9920053cbfdc0e2933ace8a286941dde17f1
Patrick Diehl <me@diehlpk.de>
2018-10-03 13:09:00 -0400

    Update cmake

--- circle_ci_scripts/build-Vc.sh
+++ circle_ci_scripts/build-Vc.sh
@@ -11,6 +11,6 @@ fi
 
 mkdir -p Vc/build
 cd Vc/build
-cmake -DCMAKE_INSTALL_PREFIX=/home/circleci/Vc_install -DCMAKE_BUILD_TYPE=release -DBUILD_TESTING=OFF ../
+/home/circleci/cmake_install/bin/cmake -DCMAKE_INSTALL_PREFIX=/home/circleci/Vc_install -DCMAKE_BUILD_TYPE=release -DBUILD_TESTING=OFF ../
 make -j2 VERBOSE=1 install
 cd ../..
--- circle_ci_scripts/build-hpx.sh
+++ circle_ci_scripts/build-hpx.sh
@@ -13,7 +13,7 @@ mkdir -p hpx/build
 cd hpx/build
 
 # detection of Vc doesn't work with a relative path
-cmake -DBOOST_ROOT=/home/circleci/boost_1_63_0_install -DHPX_WITH_EXAMPLES=OFF -DHPX_WITH_DATAPAR_VC=true -DVc_ROOT=/home/circleci/Vc_install -DCMAKE_INSTALL_PREFIX=/home/circleci/hpx_install -DCMAKE_BUILD_TYPE=release ../
+/home/circleci/cmake_install/bin/cmake -DBOOST_ROOT=/home/circleci/boost_1_63_0_install -DHPX_WITH_EXAMPLES=OFF -DHPX_WITH_DATAPAR_VC=true -DVc_ROOT=/home/circleci/Vc_install -DCMAKE_INSTALL_PREFIX=/home/circleci/hpx_install -DCMAKE_BUILD_TYPE=release ../
 
 # uses more than 4G with 4 threads (4G limit on Circle CI)
 make -j2 VERBOSE=1 install
