478fc7c3d98241724f5eb6f1550395540aa9d437
Dominic Marcello <dmarce504@gmail.com>
2018-10-11 09:31:44 -0500

    Added missing test script

--- test/gravity_test.sh
+++ test/gravity_test.sh
@@ -6,5 +6,5 @@ fi
 if [ ! -f gravity_original.silo ]; then
 wget http://phys.lsu.edu/~dmarcel/gravity_original.silo
 fi
-../octotiger -t8 -Xscale=10.0 -Xscale=4.031496063 -Problem=dwd -Eos=ideal -Max_level=8 -VariableOmega=1 -CoreRefine -Stopstep=0 -Restart=./gravity_test.chk -Ihpx.stacks.use_guard_pages=0 -Output=gravity_test
+../octotiger -t8 --xscale=4.031496063 --problem=dwd --eos=ideal --maxlevel=8 --variable_omega=1 --core_fine --stopstep=0 --restart=./gravity_test.chk -Ihpx.stacks.use_guard_pages=0 --output=gravity_test
 ../silocmp gravity_test.silo gravity_original.silo 
