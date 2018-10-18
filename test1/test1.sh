4995378ff3e2905dfb4a6896e2b1b9b17776a804
Dominic Marcello <dmarce504@gmail.com>
2018-10-18 08:59:51 -0500

    Update test1.sh

--- test1/test1.sh
+++ test1/test1.sh
@@ -1,3 +1,3 @@
-$OCTOTIGER_EXE -t2 --problem=moving_star --maxlevel=3 --odt=0.3 --stoptime=0.2 --xscale=20.0 --omega=0.1 --hpx:bind=none  --stopstep=9
+../build/octotiger -t2 --problem=moving_star --maxlevel=3 --odt=0.3 --stoptime=0.2 --xscale=20.0 --omega=0.1 --hpx:bind=none  --stopstep=9
 
 
