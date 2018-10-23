e19e2cee68b3b8237a108ba8972ecd96f4f9a3e3
Dominic Marcello <dmarce504@gmail.com>
2018-10-23 15:53:30 -0500

    Update build-octotiger.sh

--- circle_ci_scripts/build-octotiger.sh
+++ circle_ci_scripts/build-octotiger.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
 set -x
 
-export LD_LIBRARY_PATH=/home/circleci/silo_install/lib64:/home/circleci/silo_install/lib:$LD_LIBRARY_PATH
-export LIBRARY_PATH=/home/circleci/silo_install/lib64:/home/circleci/silo_install/lib:$LIBRARY_PATH
-export PATH=$HOME/home/circleci/silo_install/bin:$PATH
-export CPATH=/home/circleci/silo_install/include:$CPATH
+#export LD_LIBRARY_PATH=/home/circleci/silo_install/lib64:/home/circleci/silo_install/lib:$LD_LIBRARY_PATH
+#export LIBRARY_PATH=/home/circleci/silo_install/lib64:/home/circleci/silo_install/lib:$LIBRARY_PATH
+#export PATH=$HOME/home/circleci/silo_install/bin:$PATH
+#export CPATH=/home/circleci/silo_install/include:$CPATH
 
 mkdir build
 cd build
-cmake -DCMAKE_CXX_FLAGS="-march=native" -DBOOST_ROOT=/home/circleci/boost_1_63_0_install -DOCTOTIGER_WITH_SILO=OFF -DCMAKE_PREFIX_PATH=/home/circleci/hpx_install -DCMAKE_BUILD_TYPE=release -DHPX_IGNORE_COMPILER_COMPATIBILITY=ON -DSilo_INCLUDE_DIR=/home/circleci/silo_install/include  -DSilo_LIBRARY=/home/circleci/silo_install/lib/libsilo.a ../
+cmake -DCMAKE_CXX_FLAGS="-march=native" -DBOOST_ROOT=/home/circleci/boost_1_63_0_install -DOCTOTIGER_WITH_SILO=ON -DCMAKE_PREFIX_PATH=/home/circleci/hpx_install -DCMAKE_BUILD_TYPE=release -DHPX_IGNORE_COMPILER_COMPATIBILITY=ON -DSilo_INCLUDE_DIR=/home/circleci/silo_install/include  -DSilo_LIBRARY=/home/circleci/silo_install/lib/libsilo.a ../
 make -j2 VERBOSE=1
 cd ..
 
