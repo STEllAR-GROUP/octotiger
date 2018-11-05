404290a8e4af790be7caa443abcec28769ac8cbf
Dominic Marcello <dmarce504@gmail.com>
2018-11-05 11:42:52 -0600

    Update build-cmake.sh

--- circle_ci_scripts/build-cmake.sh
+++ circle_ci_scripts/build-cmake.sh
@@ -7,19 +7,16 @@ if [ ! -d "cmake-3.12.4/" ]; then
     wget https://cmake.org/files/v3.12/cmake-3.12.4.tar.gz
 	tar -xvzf cmake-3.12.4.tar.gz
 	cd cmake-3.12.4/
-	mkdir -p build && cd build
-    cmake -DCMAKE_INSTALL_PREFIX=/home/circleci/cmake_install ..
-	make -j 4
-    make install
-	cd ../..
-	
+	./configure --prefix=/home/circleci/cmake_install
+    make -j4 install
+	cd ..
 fi
 
 
 if [ ! -d "cmake_install" ]; then
-	cd cmake-3.12.4/build
+	cd cmake-3.12.4
 	make install
-	cd ../..
+	cd ..
 fi
 
 
