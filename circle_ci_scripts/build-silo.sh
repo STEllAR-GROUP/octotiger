df2a36ac5ea9046f7becf13866a3aab1df3e4098
Dominic Marcello <dmarce504@gmail.com>
2018-11-19 11:23:55 -0600

    Fixed bad silo download link in circleci

--- circle_ci_scripts/build-silo.sh
+++ circle_ci_scripts/build-silo.sh
@@ -5,7 +5,7 @@ if [ ! -d "silo/" ]; then
     mkdir silo
     cd silo
     if [ ! -d "silo-4.10.2" ]; then
-       wget https://wci.llnl.gov/content/assets/docs/simulation/computer-codes/silo/silo-4.10.2/silo-4.10.2.tar.gz
+       wget phys.lsu.edu/~dmarcel/silo-4.10.2.tar.gz
     fi
        tar -xvf silo-4.10.2.tar.gz
     cd ..
