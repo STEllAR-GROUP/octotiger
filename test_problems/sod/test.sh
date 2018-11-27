bdb7a38d176130c455029d34a53d170ef86b68aa
Dominic Marcello <dmarce504@gmail.com>
2018-11-27 09:00:51 -0600

    Added missing sod.ini and corrected download for original.silo

--- test_problems/sod/test.sh
+++ test_problems/sod/test.sh
@@ -1,5 +1,5 @@
 $1 --config_file=sod.ini -t2
-scp phys.lsu.edu/~dmarcel/original.silo .
+wget phys.lsu.edu/~dmarcel/original.silo
 $2 original.silo final.silo > diff.txt
 if [[ $(wc -l <diff.txt) -gt 11 ]]; then
 	echo 'Sod shock tube test failed comparison with original'
