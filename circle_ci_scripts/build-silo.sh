4ad30c94712d6d2ed4e697fc1e3f8c5ac3f0e469
Dominic Marcello <dmarce504@gmail.com>
2018-12-04 12:12:24 -0600

    Statically linking hdf5 to silo browser

--- circle_ci_scripts/build-silo.sh
+++ circle_ci_scripts/build-silo.sh
@@ -13,6 +13,8 @@ fi
 
 cd silo
 cd silo-4.10.2
+cat configure | sed 's/-lhdf5/$hdf5_lib\/libhdf5.a -ldl/g' > tmp
+mv tmp configure
 ./configure --prefix=/home/circleci/silo_install --with-hdf5=/home/circleci/hdf5_install/include,/home/circleci/hdf5_install/lib/ --enable-optimization
 make -j2 VERBOSE=1 install
 
