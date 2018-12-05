ef1e55f384db33bcf9097ca23c56b78d96309439
dmarce1 <dmarce504@gmail.com>
2018-12-04 22:14:39 -0600

    Updated test.sh

--- test_problems/blast/test.sh
+++ test_problems/blast/test.sh
@@ -1,11 +1,11 @@
-$1 --config_file=sod.ini -t2
-if [ ! -f "original.silo" ]; then
-	wget phys.lsu.edu/~dmarcel/original.silo
+$1 --config_file=blast.ini -t2
+if [ ! -f "original_blast.silo" ]; then
+	wget phys.lsu.edu/~dmarcel/original_blast.silo
 fi
-$2 -x 1.0 -R 1.0e-12 original.silo final.silo > diff.txt
+$2 -x 1.0 -R 1.0e-12 original_blast.silo final.silo > diff.txt
 cat diff.txt
 if [[ $(wc -l <diff.txt) -gt 11 ]]; then
-	echo 'Sod shock tube test failed comparison with original'
+	echo 'Blast Wave test failed comparison with original'
 	exit 1
 fi
 
