a7a0ef999e0ad2de2c536095814b3ca7a637267c
Dominic Marcello <dmarce504@gmail.com>
2018-12-04 21:59:24 -0600

    Update test.sh

--- test_problems/sod/test.sh
+++ test_problems/sod/test.sh
@@ -2,7 +2,7 @@ $1 --config_file=sod.ini -t2
 if [ ! -f "original.silo" ]; then
 	wget phys.lsu.edu/~dmarcel/original.silo
 fi
-$2 -A 1.0e-10 -R 1.0e-10 original.silo final.silo > diff.txt
+$2 -x 1.0 -R 1.0e-12 original.silo final.silo > diff.txt
 cat diff.txt
 if [[ $(wc -l <diff.txt) -gt 11 ]]; then
 	echo 'Sod shock tube test failed comparison with original'
