44221d2166ec689beec218da88807f766edbc4e8
dmarce1 <dmarce504@gmail.com>
2018-12-09 14:01:36 -0600

    Fixed tests

--- test_problems/sod/test.sh
+++ test_problems/sod/test.sh
@@ -1,8 +1,8 @@
 $1 --config_file=sod.ini -t2
-if [ ! -f "original_sod.1.silo" ]; then
-	wget phys.lsu.edu/~dmarcel/original_sod.1.silo
+if [ ! -f "sod.2.silo" ]; then
+	wget phys.lsu.edu/~dmarcel/sod.2.silo
 fi
-$2 -x 1.0 -R 1.0e-12 original_sod.1.silo final.silo > diff.txt
+$2 -x 1.0 -R 1.0e-12 sod.2.silo final.silo > diff.txt
 cat diff.txt
 if [[ $(wc -l <diff.txt) -gt 11 ]]; then
 	echo 'Sod shock tube test failed comparison with original'
