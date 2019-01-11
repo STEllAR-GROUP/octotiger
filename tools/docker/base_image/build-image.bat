9479aee2b336290c4d1323577579ff83382d0a11
Parsa Amini <parsa@cct.lsu.edu>
2019-01-11 02:45:48 -0600

    Added copyright headers

--- tools/docker/base_image/build-image.bat
+++ tools/docker/base_image/build-image.bat
@@ -1,2 +1,7 @@
+:: Copyright (c) 2018 Parsa Amini
+::
+:: Distributed under the Boost Software License, Version 1.0. (See accompanying
+:: file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 docker build -t prsam/octotiger:base_image .
 docker push prsam/octotiger:base_image
--- tools/docker/base_image/start-container.bat
+++ tools/docker/base_image/start-container.bat
@@ -1,1 +1,6 @@
+:: Copyright (c) 2018 Parsa Amini
+::
+:: Distributed under the Boost Software License, Version 1.0. (See accompanying
+:: file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 docker run -it --rm prsam/octotiger:base_image
