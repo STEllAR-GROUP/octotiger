2bbe536847dad4544a0ceb1b37bc898d98f36bd2
Parsa Amini <parsa@cct.lsu.edu>
2019-01-14 16:27:21 -0600

    Fix Dominic's build script

--- build_octotiger.sh
+++ build_octotiger.sh
@@ -16,17 +16,14 @@ rm -r CMakeFiles
 
 
 cmake -DCMAKE_PREFIX_PATH="$HOME/local/$1/hpx" -DCMAKE_CXX_FLAGS="-DBOOST_USE_VALGRIND" \
-      -DCMAKE_Fortran_COMPILER=gfortran \
       -DCMAKE_CXX_COMPILER=g++ \
       -DCMAKE_C_COMPILER=gcc \
       -DCMAKE_CXX_FLAGS="-DBOOST_USE_VALGRIND -L$HOME/local/boost/lib -march=native" \
       -DCMAKE_C_FLAGS="-L$HOME/local/boost/lib" \
       -DCMAKE_BUILD_TYPE=$1                                                                                                                            \
       -DCMAKE_INSTALL_PREFIX="$HOME/local/$1/octotiger"                                   \
-      -DHDF5_LIBRARY=$HOME/local/hdf5/lib/libhdf5.a \
-      -DHDF5_INCLUDE_DIR=$HOME/local/hdf5/include \
-      -DSilo_LIBRARY=$HOME/local/silo/lib/libsiloh5.a \
-      -DSilo_INCLUDE_DIR=$HOME/local/silo/include \
+      -DHDF5_ROOT=$HOME/local/hdf5 \
+      -DSilo_DIR=$HOME/local/silo
       ..
 
 
