bf0aa05360e6373a676e0ece9b218f74b588a0c6
Parsa Amini <parsa@users.noreply.github.com>
2019-01-15 18:59:35 -0600

    Added GCC 7 to base images (#100)

--- tools/docker/base_image/build-image.bat
+++ tools/docker/base_image/build-image.bat
@@ -4,6 +4,8 @@
 :: file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 docker build -t stellargroup/octotiger:prerequisites-gcc6 -f prerequisites-gcc6.dockerfile .
+docker build -t stellargroup/octotiger:prerequisites-gcc7 -f prerequisites-gcc7.dockerfile .
 docker build -t stellargroup/octotiger:prerequisites-gcc8 -f prerequisites-gcc8.dockerfile .
 docker push stellargroup/octotiger:prerequisites-gcc6
+docker push stellargroup/octotiger:prerequisites-gcc7
 docker push stellargroup/octotiger:prerequisites-gcc8
