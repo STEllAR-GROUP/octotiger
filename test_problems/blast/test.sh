66d7a54bfb937876b0b30407f89235697e6914b3
Dominic Marcello <dmarce504@gmail.com>
2019-01-16 09:00:28 -0600

    added more sophisticated avaluation of silo diffs for circleci tests

--- test_problems/blast/test.sh
+++ test_problems/blast/test.sh
@@ -1,12 +1,15 @@
-$1 --config_file=blast.ini 
+$1 --config_file=blast.ini
 if [ ! -f "blast.4.silo" ]; then
 	wget phys.lsu.edu/~dmarcel/blast.4.silo
 fi
 $2 -x 1.0 -R 1.0e-12 blast.4.silo final.silo > diff.txt
-cat diff.txt
-if [[ $(wc -l <diff.txt) -gt 11 ]]; then
-	echo 'Blast Wave test failed comparison with original'
+grep ".vals" diff.txt > vals.txt
+cat vals.txt
+if [[ $(wc -l < vals.txt) -gt 0 ]]; then
+	echo 'Blast test failed comparison with original'
 	exit 1
 fi
+rm vals.txt
+rm diff.txt
 
 
