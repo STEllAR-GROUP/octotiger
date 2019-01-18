bec865aefeb0ee40b8702338137242c2e6cb7481
Dominic Marcello <dmarce504@gmail.com>
2019-01-18 13:53:05 -0600

    Added marshak to circleci

--- test_problems/marshak/test.sh
+++ test_problems/marshak/test.sh
@@ -1,11 +1,12 @@
-$1 --config_file=marshak.ini -t2
-if [ ! -f "original_marshak.silo" ]; then
-	wget phys.lsu.edu/~dmarcel/original_marshak.silo
+$1 --config_file=marshak.ini -t48
+if [ ! -f "marshak.1.silo" ]; then
+	wget phys.lsu.edu/~dmarcel/marshak.1.silo
 fi
-$2 -x 1.0 -R 1.0e-12 original_marshak.silo final.silo > diff.txt
-cat diff.txt
-if [[ $(wc -l <diff.txt) -gt 11 ]]; then
-	echo 'marshak Wave test failed comparison with original'
+$2 -x 1.0 -R 1.0e-12 marshak.1.silo final.silo > diff.txt
+grep ".vals" diff.txt > vals.txt
+cat vals.txt
+if [[ $(wc -l < vals.txt) -gt 0 ]]; then
+	echo 'marshak shock tube test failed comparison with original'
 	exit 1
 fi
 
