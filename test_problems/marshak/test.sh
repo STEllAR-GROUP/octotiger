013235e12ab476a8f699d42dad432edc63e5791e
Dominic Marcello <dmarce504@gmail.com>
2019-01-23 10:59:21 -0600

    Update test.sh

--- test_problems/marshak/test.sh
+++ test_problems/marshak/test.sh
@@ -3,10 +3,10 @@ if [ ! -f "marshak.2.silo" ]; then
 	wget phys.lsu.edu/~dmarcel/marshak.2.silo
 fi
 $2 -x 1.0 -R 1.0e-12 marshak.2.silo final.silo > diff.txt
-cat diff.txt
-if [[ $(wc -l <diff.txt) -gt 11 ]]; then
+grep ".vals" diff.txt > vals.txt
+cat vals.txt
+if [[ $(wc -l < vals.txt) -gt 0 ]]; then
 	echo 'marshak Wave test failed comparison with original'
 	exit 1
 fi
-
 
