bcf92463cbe2e89bddbd47d4ea62e99679164f0d
Dominic Marcello <dmarce504@gmail.com>
2019-01-23 10:30:40 -0600

    Fixed tests

--- test_problems/marshak/test.sh
+++ test_problems/marshak/test.sh
@@ -1,8 +1,8 @@
-$1/octotiger --config_file=marshak.ini -t2
-if [ ! -f "marshak.1.silo" ]; then
-	wget phys.lsu.edu/~dmarcel/marshak.1.silo
+$1/octotiger --config_file=marshak.ini 
+if [ ! -f "marshak.2.silo" ]; then
+	wget phys.lsu.edu/~dmarcel/marshak.2.silo
 fi
-$2 -x 1.0 -R 1.0e-12 original_marshak.silo final.silo > diff.txt
+$2 -x 1.0 -R 1.0e-12 marshak.2.silo final.silo > diff.txt
 cat diff.txt
 if [[ $(wc -l <diff.txt) -gt 11 ]]; then
 	echo 'marshak Wave test failed comparison with original'
