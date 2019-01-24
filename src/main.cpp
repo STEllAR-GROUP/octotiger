c4432b3695e5ddfefecccd20944cb261ebe868d9
Parsa Amini <parsa@cct.lsu.edu>
2019-01-24 12:08:13 -0600

    Added a flag to enable/disable the Blast test

--- src/main.cpp
+++ src/main.cpp
@@ -60,12 +60,14 @@ void initialize(options _opts, std::vector<hpx::id_type> const& localities) {
 		set_problem(sod_shock_tube_init);
 		set_refine_test(refine_sod);
 		set_analytic(sod_shock_tube_analytic);
+#if defined(OCTOTIGER_HAVE_BLAST_TEST)
 	} else if (opts().problem == BLAST) {
 		grid::set_fgamma(7.0 / 5.0);
 //		opts().gravity = false;
 		set_problem(blast_wave);
 		set_refine_test(refine_blast);
 		set_analytic(blast_wave_analytic);
+#endif
 	} else if (opts().problem == STAR) {
 		grid::set_fgamma(5.0 / 3.0);
 		set_problem(star);
