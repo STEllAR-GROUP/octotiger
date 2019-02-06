0f6593a5ab72b6ef9f642462d674be5c2ee4290b
Parsa Amini <parsa@cct.lsu.edu>
2019-02-06 16:39:07 -0600

    Silence mv fails in CCI config

--- cmake/FindSilo.cmake
+++ cmake/FindSilo.cmake
@@ -11,7 +11,7 @@
 # Clifton Park, NY 12065
 # USA
 #
-# Andreas SchÃ¤fer
+# Andreas Schäfer
 # Informatik 3
 # Martensstr. 3
 # 91058 Erlangen
@@ -98,6 +98,9 @@ else()
     PATHS ${Silo_DIR}/src/silo)
   set(Silo_INCLUDE_DIR ${Silo_H_INCLUDE_DIR} ${Silo_X_INCLUDE_DIR})
 
+  unset(Silo_H_INCLUDE_DIR)
+  unset(Silo_X_INCLUDE_DIR)
+
   find_library(Silo_LIBRARY NAMES silohdf5
     PATHS ${Silo_DIR}/SiloWindows/MSVC2012/x64/Release)
     
@@ -118,15 +121,16 @@ if(Silo_INCLUDE_DIR)
   endif()
 endif()
 
-# handle the QUIETLY and REQUIRED arguments and set Silo_FOUND to TRUE if
+mark_as_advanced(
+  Silo_INCLUDE_DIR
+  Silo_LIBRARY
+  Silo_BROWSER)
+
+# Handle the QUIETLY and REQUIRED arguments and set Silo_FOUND to TRUE if
 # all listed variables are TRUE
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(Silo DEFAULT_MSG
   Silo_LIBRARY Silo_INCLUDE_DIR Silo_BROWSER)
-
-mark_as_advanced(
-  Silo_INCLUDE_DIR
-  Silo_LIBRARY)
 
 if(Silo_FOUND AND NOT TARGET Silo::silo)
   add_library(Silo::silo INTERFACE IMPORTED)
