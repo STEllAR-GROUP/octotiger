47b2a0038b81625c5ed1ea590bc887d4920cf80a
Parsa Amini <parsa@cct.lsu.edu>
2019-02-28 11:23:38 -0600

    Clean cpu_kernel.hpp

--- octotiger/radiation/cpu_kernel.hpp
+++ octotiger/radiation/cpu_kernel.hpp
@@ -14,126 +14,131 @@
 #include <array>
 #include <cassert>
 #include <cmath>
+#include <cstddef>
 #include <functional>
 #include <numeric>
 #include <string>
 #include <vector>
 
 namespace octotiger { namespace radiation {
-    template <typename F>
-    void abort_if_solver_not_converged(
-        real const eg_t0, real E0, F const test, real const E, real const eg_t)
-    {
-        // Bisection root finding method
-        // Indices of max, mid, and min
-        real de_max = eg_t0;
-        real de_mid = 0.0;
-        real de_min = -E0;
-        // Values of max, mid, and min
-        real f_min = test(de_min);
-        real f_mid = test(de_mid);
-        // Max iterations
-        constexpr std::size_t max_iterations = 50;
-        // Errors
-        real const error_tolerance = 1.0e-9;
-
-        for (std::size_t i = 0; i < max_iterations; ++i)
+    namespace detail {
+        template <typename F>
+        void abort_if_solver_not_converged(real const eg_t0, real E0,
+            F const test, real const E, real const eg_t)
         {
-            // Root solver has converged if error is smaller that error tolerance
-            real const error =
-                std::max(std::abs(f_mid), std::abs(f_min)) / (E + eg_t);
-            if (error < error_tolerance)
+            // Bisection root finding method
+            // Indices of max, mid, and min
+            real de_max = eg_t0;
+            real de_mid = 0.0;
+            real de_min = -E0;
+            // Values of max, mid, and min
+            real f_min = test(de_min);
+            real f_mid = test(de_mid);
+            // Max iterations
+            constexpr std::size_t max_iterations = 50;
+            // Errors
+            real const error_tolerance = 1.0e-9;
+
+            for (std::size_t i = 0; i < max_iterations; ++i)
             {
-                return;
+                // Root solver has converged if error is smaller that error tolerance
+                real const error =
+                    std::max(std::abs(f_mid), std::abs(f_min)) / (E + eg_t);
+                if (error < error_tolerance)
+                {
+                    return;
+                }
+
+                // If signs don't match, continue search in the lower half
+                if ((f_min < 0) != (f_mid < 0))
+                {
+                    de_max = de_mid;
+                    de_mid = 0.5 * (de_min + de_max);
+                    f_mid = test(de_mid);
+                }
+                // Continue search in the upper half
+                else
+                {
+                    de_min = de_mid;
+                    de_mid = 0.5 * (de_min + de_max);
+                    f_min = f_mid;
+                    f_mid = test(de_mid);
+                }
             }
+            // Error is not smaller that error tolerance after performed iterations. Abort.
+            printf("Implicit radiation solver failed to converge\n");
+            abort();
+        }    // abort_if_solver_not_converged
 
-            // If signs don't match, continue search in the lower half
-            if ((f_min < 0) != (f_mid < 0))
-            {
-                de_max = de_mid;
-                de_mid = 0.5 * (de_min + de_max);
-                f_mid = test(de_mid);
-            }
-            // Continue search in the upper half
-            else
-            {
-                de_min = de_mid;
-                de_mid = 0.5 * (de_min + de_max);
-                f_min = f_mid;
-                f_mid = test(de_mid);
-            }
-        }
-        // Error is not smaller that error tolerance after performed iterations. Abort.
-        printf("Implicit radiation solver failed to converge\n");
-        abort();
-    }
-
-    std::pair<real, space_vector> implicit_radiation_step(real E0, real& e0,
-        space_vector F0, space_vector u0, real rho, real mmw, real X, real Z,
-        real dt)
-    {
-        real const c = physcon().c;
-        real kp = kappa_p(rho, e0, mmw, X, Z);
-        real kr = kappa_R(rho, e0, mmw, X, Z);
-        real const rhoc2 = rho * c * c;
-
-        E0 /= rhoc2;
-        F0 = F0 / (rhoc2 * c);
-        e0 /= rhoc2;
-        u0 = u0 / c;
-        kp *= dt * c;
-        kr *= dt * c;
-
-        auto const B = [rho, mmw, c, rhoc2](real e) {
-            return (4.0 * M_PI / c) * B_p(rho, e * rhoc2, mmw) / rhoc2;
-        };
-
-        auto E = E0;
-        auto eg_t = e0 + 0.5 * (u0[0] * u0[0] + u0[1] * u0[1] + u0[2] * u0[2]);
-        auto F = F0;
-        auto u = u0;
-        real ei;
-        auto const eg_t0 = eg_t;
-
-        real u2_0 = 0.0;
-        real F2_0 = 0.0;
-        for (int d = 0; d < NDIM; d++)
+        std::pair<real, space_vector> implicit_radiation_step(real E0, real& e0,
+            space_vector F0, space_vector u0, real rho, real mmw, real X,
+            real Z, real dt)
         {
-            u2_0 += u[d] * u[d];
-            F2_0 += F[d] * F[d];
-        }
-        //	printf( "%e %e\n", (double) u2_0, (double) (F2_0/E/E));
-        auto const test = [&](real de) {
-            E = E0 + de;
-            real u2 = 0.0;
-            real udotF = 0.0;
+            real const c = physcon().c;
+            real kp = kappa_p(rho, e0, mmw, X, Z);
+            real kr = kappa_R(rho, e0, mmw, X, Z);
+            real const rhoc2 = rho * c * c;
+
+            E0 /= rhoc2;
+            F0 = F0 / (rhoc2 * c);
+            e0 /= rhoc2;
+            u0 = u0 / c;
+            kp *= dt * c;
+            kr *= dt * c;
+
+            auto const B = [rho, mmw, c, rhoc2](real e) {
+                return (4.0 * M_PI / c) * B_p(rho, e * rhoc2, mmw) / rhoc2;
+            };
+
+            auto E = E0;
+            auto eg_t =
+                e0 + 0.5 * (u0[0] * u0[0] + u0[1] * u0[1] + u0[2] * u0[2]);
+            auto F = F0;
+            auto u = u0;
+            real ei;
+            auto const eg_t0 = eg_t;
+
+            real u2_0 = 0.0;
+            real F2_0 = 0.0;
             for (int d = 0; d < NDIM; d++)
             {
-                auto const num = F0[d] + (4.0 / 3.0) * kr * E * (u0[d] + F0[d]);
-                auto const den = 1.0 + kr * (1.0 + (4.0 / 3.0) * E);
-                auto const deninv = 1.0 / den;
-                F[d] = num * deninv;
-                u[d] = u0[d] + F0[d] - F[d];
-                u2 += u[d] * u[d];
-                udotF += F[d] * u[d];
+                u2_0 += u[d] * u[d];
+                F2_0 += F[d] * F[d];
             }
-            ei = std::max(eg_t0 - E + E0 - 0.5 * u2, real(0.0));
-            real const b = B(ei);
-            real f = E - E0;
-            f += (kp * (E - b) + (kr - 2.0 * kp) * udotF);
-            eg_t = eg_t0 + E0 - E;
-            return f;
-        };
+            // printf( "%e %e\n", (double) u2_0, (double) (F2_0/E/E));
+            auto const test = [&](real de) {
+                E = E0 + de;
+                real u2 = 0.0;
+                real udotF = 0.0;
+                for (int d = 0; d < NDIM; d++)
+                {
+                    auto const num =
+                        F0[d] + (4.0 / 3.0) * kr * E * (u0[d] + F0[d]);
+                    auto const den = 1.0 + kr * (1.0 + (4.0 / 3.0) * E);
+                    auto const deninv = 1.0 / den;
+                    F[d] = num * deninv;
+                    u[d] = u0[d] + F0[d] - F[d];
+                    u2 += u[d] * u[d];
+                    udotF += F[d] * u[d];
+                }
+                ei = std::max(eg_t0 - E + E0 - 0.5 * u2, real(0.0));
+                real const b = B(ei);
+                real f = E - E0;
+                f += (kp * (E - b) + (kr - 2.0 * kp) * udotF);
+                eg_t = eg_t0 + E0 - E;
+                return f;
+            };
 
-        abort_if_solver_not_converged(eg_t0, E0, test, E, eg_t);
+            abort_if_solver_not_converged(eg_t0, E0, test, E, eg_t);
 
-        ei = eg_t - 0.5 * (u[0] * u[0] + u[1] * u[1] + u[2] * u[2]);
-        e0 = ei * rhoc2;
-        auto const dtinv = 1.0 / dt;
+            ei = eg_t - 0.5 * (u[0] * u[0] + u[1] * u[1] + u[2] * u[2]);
+            e0 = ei * rhoc2;
+            auto const dtinv = 1.0 / dt;
 
-        return std::make_pair(
-            real((E - E0) * dtinv * rhoc2), ((F - F0) * dtinv * rhoc2 * c));
-    }
+            return std::make_pair(
+                real((E - E0) * dtinv * rhoc2), ((F - F0) * dtinv * rhoc2 * c));
+        }    // implicit_radiation_step
+    }        // namespace detail
 
     template <integer er_i, integer fx_i, integer fy_i, integer fz_i>
     void radiation_cpu_kernel(integer const d, std::vector<real> const& rho,
@@ -157,7 +162,7 @@ namespace octotiger { namespace radiation {
                     real vy = sy[iiih] * deninv;
                     real vz = sz[iiih] * deninv;
 
-                    /* Compute e0 from dual energy formalism */
+                    // Compute e0 from dual energy formalism
                     real e0 = egas[iiih];
                     e0 -= 0.5 * vx * vx * den;
                     e0 -= 0.5 * vy * vy * den;
@@ -184,14 +189,14 @@ namespace octotiger { namespace radiation {
                     space_vector u1 = u0;
                     real e1 = e0;
 
-                    const auto ddt = implicit_radiation_step(E1, e1, F1, u1,
-                        den, mmw[iiir], X_spc[iiir], Z_spc[iiir], dt);
-                    const real dE_dt = ddt.first;
-                    const real dFx_dt = ddt.second[0];
-                    const real dFy_dt = ddt.second[1];
-                    const real dFz_dt = ddt.second[2];
+                    auto const ddt = detail::implicit_radiation_step(E1, e1, F1,
+                        u1, den, mmw[iiir], X_spc[iiir], Z_spc[iiir], dt);
+                    real const dE_dt = ddt.first;
+                    real const dFx_dt = ddt.second[0];
+                    real const dFy_dt = ddt.second[1];
+                    real const dFz_dt = ddt.second[2];
 
-                    /* Accumulate derivatives */
+                    // Accumulate derivatives
                     U[er_i][iiir] += dE_dt * dt;
                     U[fx_i][iiir] += dFx_dt * dt;
                     U[fy_i][iiir] += dFy_dt * dt;
@@ -202,7 +207,7 @@ namespace octotiger { namespace radiation {
                     sy[iiih] -= dFy_dt * dt * clightinv * clightinv;
                     sz[iiih] -= dFz_dt * dt * clightinv * clightinv;
 
-                    /* Find tau with dual energy formalism*/
+                    // Find tau with dual energy formalism
                     real e = egas[iiih];
                     e -= 0.5 * sx[iiih] * sx[iiih] * deninv;
                     e -= 0.5 * sy[iiih] * sy[iiih] * deninv;
@@ -241,7 +246,7 @@ namespace octotiger { namespace radiation {
                 }
             }
         }
-    }
-}}
+    }    // radiation_cpu_kernel
+}}       // namespace octotiger::radiation
 
 #endif    // RADIATION_CPU_KERNEL_HPP_
