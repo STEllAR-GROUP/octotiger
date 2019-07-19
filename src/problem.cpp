c4abbc77cd3f6d699bfadfc10166dcb7507dc14b
Dominic Marcello <dmarce504@gmail.com>
2019-07-19 08:29:36 -0500

    drawing amr bnd down middle of rotating_star

--- src/problem.cpp
+++ src/problem.cpp
@@ -161,6 +161,9 @@ bool refine_test_moving_star(integer level, integer max_level, real x, real y, r
 	bool rc = false;
 	real den_floor = opts().refinement_floor;
 	integer test_level = max_level;
+	if( x > 0 ) {
+		test_level--;
+	}
 	for (integer this_test_level = test_level; this_test_level >= 1; --this_test_level) {
 		if (U[rho_i] > den_floor) {
 			rc = rc || (level < this_test_level);
