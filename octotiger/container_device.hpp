4db7abe2a64ee1e0eaf581b7a0cd8b6433291219
Patrick Diehl <me@diehlpk.de>
2019-09-03 18:10:20 -0500

    Add Dominic as one of the authors

--- octotiger/container_device.hpp
+++ octotiger/container_device.hpp
@@ -1,4 +1,4 @@
-//  Copyright (c) 2019
+//  Copyright (c) 2019 Dominic C Marcello
 //                2007-2011 Hartmut Kaiser
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
