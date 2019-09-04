0a095db9d1b8ef19f8a6c4f7fae58c013ec47de3
Patrick Diehl <patrickdiehl@lsu.edu>
2019-09-04 08:43:20 -0500

    Add some of the previous authors

--- octotiger/config/export_definitions.hpp
+++ octotiger/config/export_definitions.hpp
@@ -1,4 +1,6 @@
 //  Copyright (c) 2019 Dominic C Marcello
+//                2007-2012 Hartmut Kaiser
+//                2011 Bryce Lelbach
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
