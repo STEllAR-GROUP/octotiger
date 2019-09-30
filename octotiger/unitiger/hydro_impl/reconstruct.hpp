e74d00ce47897042a428e5e6e10c66c714e25fc3
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2019-09-30 17:46:06 +0200

    Added SoA types to reconstruct

--- octotiger/unitiger/hydro_impl/reconstruct.hpp
+++ octotiger/unitiger/hydro_impl/reconstruct.hpp
@@ -7,12 +7,22 @@
 #include "octotiger/unitiger/physics.hpp"
 #include "octotiger/unitiger/physics_impl.hpp"
 
+#include <octotiger/cuda_util/cuda_helper.hpp>
+#include <octotiger/cuda_util/cuda_scheduler.hpp>
+
 
 template<int NDIM, int INX>
 const hydro::recon_type<NDIM>& hydro_computer<NDIM, INX>::reconstruct(hydro::state_type &U_, const hydro::x_type &X, safe_real omega) {
 
 	static thread_local auto D1 = std::vector<std::array<safe_real, geo::NDIR / 2>>(geo::H_N3);
 	static thread_local auto Q = std::vector < std::vector<std::array<safe_real, geo::NDIR>> > (nf_, std::vector<std::array<safe_real, geo::NDIR>>(geo::H_N3));
+
+	static thread_local octotiger::fmm::struct_of_array_data<std::array<safe_real, geo::NDIR>, real, geo::NDIR, geo::H_N3, 19,
+		std::vector<real, octotiger::fmm::cuda_pinned_allocator<real>>>
+		D1_SoA;
+
+	static thread_local std::vector<octotiger::fmm::struct_of_array_data<std::array<safe_real, geo::NDIR>, real, geo::NDIR, geo::H_N3, 19,
+		std::vector<real, octotiger::fmm::cuda_pinned_allocator<real>>>> Q_SoA(nf_);
 
 	static constexpr auto xloc = geo::xloc();
 	static constexpr auto kdelta = geo::kronecker_delta();
