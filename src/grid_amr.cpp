e3e698cbe8a2e79b1188afe9ee4cf28abdd78385
Dominic Marcello <dmarce504@gmail.com>
2019-10-10 10:09:28 -0500

    Added matching flux terms to angmom

--- src/grid_amr.cpp
+++ src/grid_amr.cpp
@@ -75,32 +75,8 @@ void grid::complete_hydro_amr_boundary() {
 	} else {
 		using oct_array = std::array<std::array<std::array<double, 2>, 2>, 2>;
 		static thread_local std::vector<std::vector<oct_array>> Uf(opts().n_fields, std::vector<oct_array>(HS_N3));
-		static thread_local std::vector<std::vector<double>> slpx(opts().n_fields, std::vector<double>(HS_N3));
-		static thread_local std::vector<std::vector<double>> slpy(opts().n_fields, std::vector<double>(HS_N3));
-		static thread_local std::vector<std::vector<double>> slpz(opts().n_fields, std::vector<double>(HS_N3));
 
-		const auto slopes1 = [this](int f) {
-			for (int i0 = 1; i0 < HS_NX - 1; i0++) {
-				for (int j0 = 1; j0 < HS_NX - 1; j0++) {
-					for (int k0 = 1; k0 < HS_NX - 1; k0++) {
-						const int iii0 = hSindex(i0, j0, k0);
-						if (is_coarse[iii0]) {
-							const auto &u0 = Ushad[f][iii0];
-							const auto &uc = Ushad[f];
-							constexpr auto theta = 2.0;
-							const auto s_x = minmod_theta(uc[iii0 + HS_DNX] - u0, u0 - uc[iii0 - HS_DNX], theta);
-							const auto s_y = minmod_theta(uc[iii0 + HS_DNY] - u0, u0 - uc[iii0 - HS_DNY], theta);
-							const auto s_z = minmod_theta(uc[iii0 + HS_DNZ] - u0, u0 - uc[iii0 - HS_DNZ], theta);
-							slpx[f][iii0] = s_x;
-							slpy[f][iii0] = s_y;
-							slpz[f][iii0] = s_z;
-						}
-					}
-				}
-			}
-		};
-
-		const auto slopes2 = [this](int f) {
+		const auto prolong = [this](int f) {
 			for (int i0 = 1; i0 < HS_NX - 1; i0++) {
 				for (int j0 = 1; j0 < HS_NX - 1; j0++) {
 					for (int k0 = 1; k0 < HS_NX - 1; k0++) {
@@ -114,18 +90,15 @@ void grid::complete_hydro_amr_boundary() {
 										const auto ks = kr % 2 ? +1 : -1;
 										const auto &u0 = Ushad[f][iii0];
 										const auto &uc = Ushad[f];
-										constexpr auto theta = 1.0;
-										const auto s_x = is * slpx[f][iii0];
-										const auto s_y = js * slpy[f][iii0];
-										const auto s_z = ks * slpz[f][iii0];
-										const auto s_xy = minmod_theta(uc[iii0 + is * HS_DNX + js * HS_DNY] - u0, u0 - uc[iii0 - is * HS_DNX - js * HS_DNY],
-												theta);
-										const auto s_xz = minmod_theta(uc[iii0 + is * HS_DNX + ks * HS_DNZ] - u0, u0 - uc[iii0 - is * HS_DNX - ks * HS_DNZ],
-												theta);
-										const auto s_yz = minmod_theta(uc[iii0 + js * HS_DNY + ks * HS_DNZ] - u0, u0 - uc[iii0 - js * HS_DNY - ks * HS_DNZ],
-												theta);
-										const auto s_xyz = minmod_theta(uc[iii0 + is * HS_DNX + js * HS_DNY + ks * HS_DNZ] - u0,
-												u0 - uc[iii0 - is * HS_DNX - js * HS_DNY - ks * HS_DNZ], theta);
+										constexpr auto theta = 2.0;
+										const auto s_x = minmod_theta(uc[iii0 + is * HS_DNX] - u0, u0 - uc[iii0 - is * HS_DNX], theta);
+										const auto s_y = minmod_theta(uc[iii0 + js * HS_DNY] - u0, u0 - uc[iii0 - js * HS_DNY], theta);
+										const auto s_z = minmod_theta(uc[iii0 + ks * HS_DNZ] - u0, u0 - uc[iii0 - ks * HS_DNZ], theta);
+										const auto s_xy = minmod(uc[iii0 + is * HS_DNX + js * HS_DNY] - u0, u0 - uc[iii0 - is * HS_DNX - js * HS_DNY]);
+										const auto s_xz = minmod(uc[iii0 + is * HS_DNX + ks * HS_DNZ] - u0, u0 - uc[iii0 - is * HS_DNX - ks * HS_DNZ]);
+										const auto s_yz = minmod(uc[iii0 + js * HS_DNY + ks * HS_DNZ] - u0, u0 - uc[iii0 - js * HS_DNY - ks * HS_DNZ]);
+										const auto s_xyz = minmod(uc[iii0 + is * HS_DNX + js * HS_DNY + ks * HS_DNZ] - u0,
+												u0 - uc[iii0 - is * HS_DNX - js * HS_DNY - ks * HS_DNZ]);
 										auto &uf = Uf[f][iii0][ir][jr][kr];
 										uf = u0;
 										uf += (9.0 / 64.0) * (s_x + s_y + s_z);
@@ -140,39 +113,8 @@ void grid::complete_hydro_amr_boundary() {
 			}
 		};
 
-		for (int f = 0; f < sx_i; f++) {
-			slopes1(f);
-			slopes2(f);
-		}
-
-		for (int f = sx_i; f <= sz_i; f++) {
-			slopes1(f);
-		}
-
-		for (int i0 = 1; i0 < HS_NX - 1; i0++) {
-			for (int j0 = 1; j0 < HS_NX - 1; j0++) {
-				for (int k0 = 1; k0 < HS_NX - 1; k0++) {
-					const int iii0 = hSindex(i0, j0, k0);
-					if (is_coarse[iii0]) {
-						const auto sym_xy = 0.5 * (slpx[sy_i][iii0] + slpy[sx_i][iii0]);
-						const auto sym_xz = 0.5 * (slpx[sz_i][iii0] + slpz[sx_i][iii0]);
-						const auto sym_yz = 0.5 * (slpy[sz_i][iii0] + slpz[sy_i][iii0]);
-						const auto ant_xy = +3.0 * Ushad[zz_i][iii0] / (dx);
-						const auto ant_xz = -3.0 * Ushad[zy_i][iii0] / (dx);
-						const auto ant_yz = +3.0 * Ushad[zx_i][iii0] / (dx);
-						slpx[sy_i][iii0] = minmod(sym_xy + ant_xy, slpx[sy_i][iii0]);
-						slpy[sx_i][iii0] = minmod(sym_xy - ant_xy, slpy[sx_i][iii0]);
-						slpx[sz_i][iii0] = minmod(sym_xz + ant_xz, slpx[sz_i][iii0]);
-						slpz[sx_i][iii0] = minmod(sym_xz - ant_xz, slpz[sx_i][iii0]);
-						slpy[sz_i][iii0] = minmod(sym_yz + ant_yz, slpy[sz_i][iii0]);
-						slpz[sy_i][iii0] = minmod(sym_yz - ant_yz, slpz[sy_i][iii0]);
-					}
-				}
-			}
-		}
-
-		for (int f = sx_i; f <= sz_i; f++) {
-			slopes2(f);
+		for (int f = 0; f <= sz_i; f++) {
+			prolong(f);
 		}
 
 		for (int i0 = 1; i0 < HS_NX - 1; i0++) {
@@ -201,8 +143,7 @@ void grid::complete_hydro_amr_boundary() {
 		}
 
 		for (int f = sz_i + 1; f < opts().n_fields; f++) {
-			slopes1(f);
-			slopes2(f);
+			prolong(f);
 		}
 
 		for (int f = 0; f < opts().n_fields; f++) {
