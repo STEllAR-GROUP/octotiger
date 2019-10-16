d6cc03aea11da360f1301f7114f231460848a826
Dominic Marcello <dmarce504@gmail.com>
2019-10-16 14:28:35 -0500

    Removed more un-needed memory allocations

--- octotiger/unitiger/physics_impl.hpp
+++ octotiger/unitiger/physics_impl.hpp
@@ -174,10 +174,11 @@ void physics<NDIM>::post_angmom(const hydro::state_type &U, const hydro::recon_t
 
 template<int NDIM>
 template<int INX>
-const hydro::state_type physics<NDIM>::pre_recon(const hydro::state_type &U, const hydro::x_type X, safe_real omega, bool angmom) {
+const hydro::state_type& physics<NDIM>::pre_recon(const hydro::state_type &U, const hydro::x_type X, safe_real omega, bool angmom) {
 	static const cell_geometry<NDIM, INX> geo;
 	static const auto indices = geo.find_indices(0, geo.H_NX);
-	auto V = U;
+	static thread_local hydro::state_type V;
+	V = U;
 	const auto dx = X[0][geo.H_DNX] - X[0][0];
 	for (int j = 0; j < geo.H_NX_X; j++) {
 		for (int k = 0; k < geo.H_NX_Y; k++) {
@@ -213,11 +214,10 @@ const hydro::state_type physics<NDIM>::pre_recon(const hydro::state_type &U, con
 
 template<int NDIM>
 template<int INX>
-std::vector<std::vector<std::vector<safe_real>>> physics<NDIM>::post_recon(const std::vector<std::vector<std::vector<safe_real>>> &P, const hydro::x_type X,
+void physics<NDIM>::post_recon(std::vector<std::vector<std::vector<safe_real>>> &Q, const hydro::x_type X,
 		safe_real omega, bool angmom) {
 	static const cell_geometry<NDIM, INX> geo;
 	static const auto indices = geo.find_indices(2, geo.H_NX - 2);
-	auto Q = P;
 	const auto dx = X[0][geo.H_DNX] - X[0][0];
 	const auto xloc = geo.xloc();
 	for (int d = 0; d < geo.NDIR; d++) {
@@ -261,7 +261,6 @@ std::vector<std::vector<std::vector<safe_real>>> physics<NDIM>::post_recon(const
 			}
 		}
 	}
-	return Q;
 }
 
 template<int NDIM>
