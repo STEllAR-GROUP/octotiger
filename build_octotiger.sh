e5a482bf76986c2ea83d29c0d2ba771a97fdba90
Dominic Marcello <dmarce504@gmail.com>
2019-11-05 15:31:45 -0600

    Made silo_convert dependent on HPX

--- build_octotiger.sh
+++ build_octotiger.sh
@@ -19,8 +19,8 @@ cmake -DCMAKE_PREFIX_PATH="$HOME/local/$1/hpx" -DCMAKE_CXX_FLAGS="-DBOOST_USE_VA
       -DCMAKE_CXX_COMPILER=g++ -DOCTOTIGER_WITH_TESTS=OFF \
       -DCMAKE_C_COMPILER=gcc \
       -DOCTOTIGER_WITH_CUDA=OFF \
-      -DCMAKE_CXX_FLAGS="-pg -DBOOST_USE_VALGRIND -L$HOME/local/boost/lib -march=native" \
-      -DCMAKE_C_FLAGS="-pg -L$HOME/local/boost/lib" \
+      -DCMAKE_CXX_FLAGS="-DBOOST_USE_VALGRIND -L$HOME/local/boost/lib -march=native" \
+      -DCMAKE_C_FLAGS="-L$HOME/local/boost/lib" \
       -DCMAKE_BUILD_TYPE=$1                                                                                                                            \
       -DCMAKE_INSTALL_PREFIX="$HOME/local/$1/octotiger"                                   \
       -DBOOST_ROOT=$HOME/local/boost \
