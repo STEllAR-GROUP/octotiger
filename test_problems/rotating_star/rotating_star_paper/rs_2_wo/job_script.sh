00ca57ad318e2f24044da426fd79a38b910e3b38
Dominic Marcello <dmarce504@gmail.com>
2019-11-27 13:03:56 -0600

    Added test configs for rotating star

---
+++ test_problems/rotating_star/rotating_star_paper/rs_2_wo/job_script.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+#PBS -j oe
+#PBS -A loni_lrn08
+#PBS -l nodes=1:ppn=20
+#PBS -l walltime=8:00:00
+#PBS -q checkpt
+
+BUILD_TYPE=relwithdebinfo
+source ~/scripts/source_all.sh $BUILD_TYPE
+
+set -x 
+
+
+cd $WORK/rotating_star_paper/rs_2_wo
+cat $PBS_NODEFILE | awk 'NR % 20 == 0' > node.list
+export HPX_NODEFILE=node.list
+unset PBS_NODEFILE
+export NPROCS=`wc -l $HPX_NODEFILE |gawk '//{print $1}'`
+
+mpirun -np $NPROCS --machinefile $HPX_NODEFILE  /home/dmarce1/local/$BUILD_TYPE/octotiger/bin/octotiger --hpx:threads 20 --config_file=config.ini
+ 
+
+
+
+
