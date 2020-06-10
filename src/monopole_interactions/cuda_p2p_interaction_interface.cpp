3a9e25262112a96de442328fcc5aee20be2a737c
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-06-10 21:10:08 +0200

    Using aliases in cuda p2p interface

--- src/monopole_interactions/cuda_p2p_interaction_interface.cpp
+++ src/monopole_interactions/cuda_p2p_interaction_interface.cpp
@@ -12,84 +12,65 @@
 #include <array>
 #include <vector>
 
-#include <cuda_runtime.h>
 #include <buffer_manager.hpp>
 #include <cuda_buffer_util.hpp>
+#include <cuda_runtime.h>
 
-namespace octotiger { namespace fmm { namespace monopole_interactions {
-    cuda_p2p_interaction_interface::cuda_p2p_interaction_interface()
-      : p2p_interaction_interface()
-      , theta(opts().theta)
-    {
-    }
+namespace octotiger {
+namespace fmm {
+    namespace monopole_interactions {
+        cuda_p2p_interaction_interface::cuda_p2p_interaction_interface()
+          : p2p_interaction_interface()
+          , theta(opts().theta) {}
 
-    void cuda_p2p_interaction_interface::compute_p2p_interactions(
-        std::vector<real>& monopoles,
-        std::vector<neighbor_gravity_type>& neighbors, gsolve_type type,
-        real dx, std::array<bool, geo::direction::count()>& is_direction_empty)
-    {
-        kernel_scheduler::scheduler().init();
-        // Check where we want to run this:
-        int slot = kernel_scheduler::scheduler().get_launch_slot();
-        if (slot == -1 || p2p_type == interaction_kernel_type::OLD)
-        {
-            // Run CPU implementation
-            p2p_interaction_interface::compute_p2p_interactions(
-                monopoles, neighbors, type, dx, is_direction_empty);
+        void cuda_p2p_interaction_interface::compute_p2p_interactions(std::vector<real>& monopoles,
+            std::vector<neighbor_gravity_type>& neighbors, gsolve_type type, real dx,
+            std::array<bool, geo::direction::count()>& is_direction_empty) {
+            kernel_scheduler::scheduler().init();
+            // Check where we want to run this:
+            int slot = kernel_scheduler::scheduler().get_launch_slot();
+            if (slot == -1 || p2p_type == interaction_kernel_type::OLD) {
+                // Run CPU implementation
+                p2p_interaction_interface::compute_p2p_interactions(
+                    monopoles, neighbors, type, dx, is_direction_empty);
+            } else {
+                // run on CUDA device
+                cuda_launch_counter()++;
+
+                cuda_expansion_result_buffer_t potential_expansions_SoA;
+                cuda_monopole_buffer_t local_monopoles(ENTRIES);
+                recycler::cuda_device_buffer<double> device_local_monopoles(ENTRIES);
+                recycler::cuda_device_buffer<double> erg(NUMBER_POT_EXPANSIONS_SMALL);
+
+                // Move data into staging buffers
+                update_input(monopoles, neighbors, type, local_monopoles);
+
+                // Queue moving of input data to device
+                util::cuda_helper& gpu_interface =
+                    kernel_scheduler::scheduler().get_launch_interface(slot);
+                gpu_interface.copy_async(device_local_monopoles.device_side_buffer,
+                    local_monopoles.data(), local_monopoles_size, cudaMemcpyHostToDevice);
+
+                // Launch kernel and queue copying of results
+                dim3 const grid_spec(INX / 2, 1, 1);
+                dim3 const threads_per_block(1, INX, INX);
+                void* args[] = {&(device_local_monopoles.device_side_buffer),
+                    &(erg.device_side_buffer), &theta, &dx};
+                gpu_interface.execute(reinterpret_cast<void const*>(&cuda_p2p_interactions_kernel),
+                    grid_spec, threads_per_block, args, 0);
+                gpu_interface.copy_async(potential_expansions_SoA.get_pod(), erg.device_side_buffer,
+                    potential_expansions_small_size, cudaMemcpyDeviceToHost);
+
+                // Wait for stream to finish and allow thread to jump away in the meantime
+                auto fut = gpu_interface.get_future();
+                fut.get();
+
+                // Copy results back into non-SoA array
+                potential_expansions_SoA.add_to_non_SoA(grid_ptr->get_L());
+            }
         }
-        else
-        {
 
-            struct_of_array_data<expansion, real, 20, INNER_CELLS, SOA_PADDING,
-                std::vector<double, recycler::recycle_allocator_cuda_host<double>>>
-                potential_expansions_SoA;
-            // std::vector<double, recycler::recycle_allocator_cuda_host<double>> test;
-
-            // run on CUDA device
-            cuda_launch_counter()++;
-
-            std::vector<double, recycler::recycle_allocator_cuda_host<double>> local_monopoles(ENTRIES);
-            recycler::cuda_device_buffer<double> device_local_monopoles(ENTRIES);
-            recycler::cuda_device_buffer<double> erg(NUMBER_POT_EXPANSIONS_SMALL);
-
-            // Move data into staging arrays
-            update_input(
-                monopoles, neighbors, type, local_monopoles);
-
-            // Queue moving of input data to device
-            util::cuda_helper& gpu_interface =
-                kernel_scheduler::scheduler().get_launch_interface(slot);
-            gpu_interface.copy_async(device_local_monopoles.device_side_buffer,
-                local_monopoles.data(), local_monopoles_size,
-                cudaMemcpyHostToDevice);
-
-            // Launch kernel and queue copying of results
-            // const dim3 grid_spec(3);
-            dim3 const grid_spec(INX / 2, 1, 1);
-            dim3 const threads_per_block(1, INX, INX);
-            // void* args[] = {&(env.device_local_monopoles), &(env.device_blocked_monopoles),
-            //                 &theta, &dx};
-            void* args[] = {&(device_local_monopoles.device_side_buffer),
-                &(erg.device_side_buffer), &theta, &dx};
-            gpu_interface.execute(reinterpret_cast<void const*>(&cuda_p2p_interactions_kernel),
-                grid_spec, threads_per_block, args, 0);
-            // void* sum_args[] = {&(env.device_blocked_monopoles)};
-            // dim3 const sum_spec(1);
-            // dim3 const threads(512);
-            // gpu_interface.execute(
-            //     &cuda_add_pot_blocks, sum_spec, threads, sum_args, 0);
-            gpu_interface.copy_async(potential_expansions_SoA.get_pod(),
-                erg.device_side_buffer, potential_expansions_small_size,
-                cudaMemcpyDeviceToHost);
-
-            // Wait for stream to finish and allow thread to jump away in the meantime
-            auto fut = gpu_interface.get_future();
-            fut.get();
-
-            // Copy results back into non-SoA array
-            potential_expansions_SoA.add_to_non_SoA(grid_ptr->get_L());
-        }
-    }
-
-}}}
+    }    // namespace monopole_interactions
+}    // namespace fmm
+}    // namespace octotiger
 #endif
