9650a87db62ad52b66b6ce97002fe425b418de57
Dominic Marcello <dmarce504@gmail.com>
2020-06-15 16:24:37 -0500

    Added inflow bc

--- octotiger/options.hpp
+++ octotiger/options.hpp
@@ -24,12 +24,13 @@
  COMMAND_LINE_ENUM(eos_type,IDEAL,WD);
  */
 
-COMMAND_LINE_ENUM(problem_type,DWD,SOD,BLAST,NONE,SOLID_SPHERE,STAR,MOVING_STAR,RADIATION_TEST,ROTATING_STAR,MARSHAK,AMR_TEST,ADVECTION);
+COMMAND_LINE_ENUM(problem_type, DWD, SOD, BLAST, NONE, SOLID_SPHERE, STAR, MOVING_STAR, RADIATION_TEST, ROTATING_STAR, MARSHAK, AMR_TEST, ADVECTION);
 
 COMMAND_LINE_ENUM(eos_type, IDEAL, WD);
 
 class options {
 public:
+	bool inflow_bc;
 	bool reflect_bc;
 	int experiment;
 	bool cdisc_detect;
@@ -96,15 +97,15 @@ public:
 	real sod_phi;
 	real sod_gamma;
 
-        real star_xcenter;
-        real star_ycenter;
-        real star_zcenter;
-        real star_rmax;
-        real star_alpha;
-        real star_rho_out;
-        real star_dr;
-        real star_n;
-        real star_rho_center;
+	real star_xcenter;
+	real star_ycenter;
+	real star_zcenter;
+	real star_rmax;
+	real star_alpha;
+	real star_rho_out;
+	real star_dr;
+	real star_n;
+	real star_rho_center;
 
 	size_t cuda_streams_per_locality;
 	size_t cuda_streams_per_gpu;
@@ -133,7 +134,7 @@ public:
 
 	template<class Arc>
 	void serialize(Arc &arc, unsigned) {
-		arc &eblast0;
+		arc & eblast0;
 		arc & rho_floor;
 		arc & tau_floor;
 		arc & sod_rhol;
@@ -143,15 +144,16 @@ public:
 		arc & sod_theta;
 		arc & sod_phi;
 		arc & sod_gamma;
-                arc & star_xcenter;
-                arc & star_ycenter;
-                arc & star_zcenter;
-                arc & star_rmax;
-                arc & star_alpha;
-                arc & star_dr;
-                arc & star_n;
-                arc & star_rho_center;
-                arc & star_rho_out;
+		arc & star_xcenter;
+		arc & star_ycenter;
+		arc & star_zcenter;
+		arc & star_rmax;
+		arc & star_alpha;
+		arc & star_dr;
+		arc & star_n;
+		arc & star_rho_center;
+		arc & star_rho_out;
+		arc & inflow_bc;
 		arc & reflect_bc;
 		arc & cdisc_detect;
 		arc & experiment;
