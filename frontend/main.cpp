ce340713d755fc7c37459cd183c294197ebe7523
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-07-03 00:32:21 +0200

    Use callback executors for now

--- frontend/main.cpp
+++ frontend/main.cpp
@@ -238,7 +238,7 @@ void initialize(options _opts, std::vector<hpx::id_type> const& localities) {
 
 #ifdef OCTOTIGER_HAVE_CUDA
 	std::cout << "CUDA is enabled! Available CUDA targets on this locality: " << std::endl;
-    stream_pool::init<hpx::cuda::cuda_executor, pool_strategy>(opts().cuda_streams_per_gpu,opts().cuda_number_gpus);
+    stream_pool::init<hpx::cuda::cuda_executor, pool_strategy>(opts().cuda_streams_per_gpu,opts().cuda_number_gpus, false);
     octotiger::fmm::kernel_scheduler::init_constants();
 #endif
 	grid::static_init();
