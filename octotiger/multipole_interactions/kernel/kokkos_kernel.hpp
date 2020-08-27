2072ea347d26690eace6972b0ef134765770c6bd
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-08-28 00:25:14 +0200

    Added basic buffer management for kokkos multipoles

--- octotiger/multipole_interactions/kernel/kokkos_kernel.hpp
+++ octotiger/multipole_interactions/kernel/kokkos_kernel.hpp
@@ -7,8 +7,6 @@
 #include "octotiger/common_kernel/kokkos_util.hpp"
 #endif
 
-// TODO(daissgr) Change to correct parameter lists
-
 // --------------------------------------- Stencil interface
 
 // TODO(daissgr) Return stencils
@@ -19,8 +17,7 @@
 template <typename executor_t, typename kokkos_buffer_t, typename kokkos_mask_t>
 void multipole_kernel_rho_impl(executor_t& exec, kokkos_buffer_t& monopoles,
     kokkos_buffer_t& centers_of_mass, kokkos_buffer_t& multipoles,
-    kokkos_buffer_t& potential_expansions, kokkos_buffer_t& angular_corrections, 
-    double theta) {
+    kokkos_buffer_t& potential_expansions, kokkos_buffer_t& angular_corrections, double theta) {
     static_assert(always_false<executor_t>::value,
         "Multipole Rho Kernel not implemented for this kind of executor!");
 }
@@ -28,8 +25,7 @@ void multipole_kernel_rho_impl(executor_t& exec, kokkos_buffer_t& monopoles,
 template <typename kokkos_backend_t, typename kokkos_buffer_t, typename kokkos_mask_t>
 void multipole_kernel_rho_impl(hpx::kokkos::executor<kokkos_backend_t>& executor,
     kokkos_buffer_t& monopoles, kokkos_buffer_t& centers_of_mass, kokkos_buffer_t& multipoles,
-    kokkos_buffer_t& potential_expansions, kokkos_buffer_t& angular_corrections, 
-    double theta) {
+    kokkos_buffer_t& potential_expansions, kokkos_buffer_t& angular_corrections, double theta) {
     using namespace octotiger::fmm;
 
     Kokkos::MDRangePolicy<decltype(executor.instance()), Kokkos::Rank<3>> policy_1(
@@ -38,8 +34,8 @@ void multipole_kernel_rho_impl(hpx::kokkos::executor<kokkos_backend_t>& executor
 // --------------------------------------- Kernel non rho implementations
 
 template <typename executor_t, typename kokkos_buffer_t, typename kokkos_mask_t>
-void multipole_kernel_non_rho_impl(executor_t& exec,
-    kokkos_buffer_t& monopoles, kokkos_buffer_t& centers_of_mass, kokkos_buffer_t& multipoles,
+void multipole_kernel_non_rho_impl(executor_t& exec, kokkos_buffer_t& monopoles,
+    kokkos_buffer_t& centers_of_mass, kokkos_buffer_t& multipoles,
     kokkos_buffer_t& potential_expansions, double theta) {
     static_assert(always_false<executor_t>::value,
         "Mutlipole Non-Rho Kernel not implemented for this kind of executor!");
@@ -62,7 +58,7 @@ template <typename executor_t>
 void launch_interface(executor_t& exec, host_buffer<double>& monopoles,
     host_buffer<double>& centers_of_mass, host_buffer<double>& multipoles,
     host_buffer<double>& potential_expansions, host_buffer<double>& angular_corrections,
-    double theta) {
+    double theta, gsolve_type type) {
     static_assert(always_false<executor_t>::value,
         "Multipole launch interface implemented for this kind of executor!");
 }
@@ -71,22 +67,72 @@ template <typename kokkos_backend_t>
 void launch_interface(hpx::kokkos::executor<kokkos_backend_t>& exec, host_buffer<double>& monopoles,
     host_buffer<double>& centers_of_mass, host_buffer<double>& multipoles,
     host_buffer<double>& potential_expansions, host_buffer<double>& angular_corrections,
-    double theta) {
-    // TODO(daissgr) Implement generic kokkos launch interface
+    double theta, gsolve_type type) {
+    // TODO(daissgr) Get masks
+    // input buffers
+    device_buffer<double> device_monopoles(octotiger::fmm::NUMBER_LOCAL_MONOPOLE_VALUES);
+    Kokkos::deep_copy(exec.instance(), device_monopoles, monopoles);
+    device_buffer<double> device_multipoles(octotiger::fmm::NUMBER_LOCAL_EXPANSION_VALUES);
+    Kokkos::deep_copy(exec.instance(), device_multipoles, multipoles);
+    device_buffer<double> device_centers(octotiger::fmm::NUMBER_MASS_VALUES);
+    Kokkos::deep_copy(exec.instance(), device_centers, centers_of_mass);
+    // result buffers
+    device_buffer<double> device_expansions(octotiger::fmm::NUMBER_POT_EXPANSIONS);
+    device_buffer<double> device_corrections(octotiger::fmm::NUMBER_ANG_CORRECTIONS);
+    if (type == RHO) {
+        // Launch kernel with angular corrections
+        multipole_kernel_rho_impl(exec, device_monopoles, device_centers, device_multipoles,
+            device_expansions, device_corrections, theta);
+        // Copy back angular cocrection results
+        Kokkos::deep_copy(exec.instance(), angular_corrections, device_corrections);
+    } else {
+        // Launch kernel without angular corrections
+        multipole_kernel_non_rho_impl(
+            exec, device_monopoles, device_centers, device_multipoles, device_expansions, theta);
+    }
+    // Copy back potential expansions results and sync
+    auto fut =
+        hpx::kokkos::deep_copy_async(exec.instance(), potential_expansions, device_expansions);
+    fut.get();
 }
 template <>
 void launch_interface(hpx::kokkos::executor<Kokkos::Serial>& exec, host_buffer<double>& monopoles,
     host_buffer<double>& centers_of_mass, host_buffer<double>& multipoles,
     host_buffer<double>& potential_expansions, host_buffer<double>& angular_corrections,
-    double theta) {
-    // TODO(daissgr) Implement serial kokkos launch interface
+    double theta, gsolve_type type) {
+    // TODO(daissgr) Get masks
+    if (type == RHO) {
+        // Launch kernel with angular corrections
+        multipole_kernel_rho_impl<Kokkos::Serial, host_buffer<double>, host_buffer<bool>>(exec,
+            monopoles, centers_of_mass, multipoles, potential_expansions, angular_corrections,
+            theta);
+    } else {
+        // Launch kernel without angular corrections
+        multipole_kernel_non_rho_impl<Kokkos::Serial, host_buffer<double>, host_buffer<bool>>(
+            exec, monopoles, centers_of_mass, multipoles, potential_expansions, theta);
+    }
+    // Sync
+    exec.instance().fence();
 }
 template <>
 void launch_interface(hpx::kokkos::executor<Kokkos::Experimental::HPX>& exec,
     host_buffer<double>& monopoles, host_buffer<double>& centers_of_mass,
     host_buffer<double>& multipoles, host_buffer<double>& potential_expansions,
-    host_buffer<double>& angular_corrections, double theta) {
-    // TODO(daissgr) Implement hpx kokkos launch interface
+    host_buffer<double>& angular_corrections, double theta, gsolve_type type) {
+    // TODO(daissgr) Get masks
+    if (type == RHO) {
+        // Launch kernel with angular corrections
+        multipole_kernel_rho_impl<Kokkos::Experimental::HPX, host_buffer<double>,
+            host_buffer<bool>>(exec, monopoles, centers_of_mass, multipoles, potential_expansions,
+            angular_corrections, theta);
+    } else {
+        // Launch kernel without angular corrections
+        multipole_kernel_non_rho_impl<Kokkos::Experimental::HPX, host_buffer<double>,
+            host_buffer<bool>>(
+            exec, monopoles, centers_of_mass, multipoles, potential_expansions, theta);
+    }
+    // Sync
+    exec.instance().fence();
 }
 
 // --------------------------------------- Kernel interface
@@ -97,7 +143,18 @@ void multipole_kernel(executor_t& exec, std::vector<real>& monopoles, std::vecto
     std::vector<neighbor_gravity_type>& neighbors, gsolve_type type, real dx,
     std::array<bool, geo::direction::count()>& is_direction_empty, std::array<real, NDIM> xbase,
     std::shared_ptr<grid> grid) {
-    // TODO (daissgr) Create required host buffers
-    // TODO (daissgr) Call launch interface
+    // input buffers
+    host_buffer<double> host_monopoles(octotiger::fmm::NUMBER_LOCAL_MONOPOLE_VALUES);
+    host_buffer<double> host_multipoles(octotiger::fmm::NUMBER_LOCAL_EXPANSION_VALUES);
+    host_buffer<double> host_masses(octotiger::fmm::NUMBER_MASS_VALUES);
+    // result buffers
+    host_buffer<double> host_expansions(octotiger::fmm::NUMBER_POT_EXPANSIONS);
+    host_buffer<double> host_corrections(octotiger::fmm::NUMBER_ANG_CORRECTIONS);
+    // convert input AoS into SoA input buffers
+    octotiger::fmm::multipole_interactions::update_input(monopoles, M_ptr, com_ptr, neighbors, type, dx, xbase, host_monopoles,
+        host_multipoles, host_masses, grid);
+    // launch kernel (and copy data to device if necessary)
+    launch_interface(exec, host_monopoles, host_masses, host_multipoles, host_expansions,
+        host_corrections, dx, type); // TODO(daissgr) this needs theta, not dx
     // TODO (daissgr) Copy results back
 }
