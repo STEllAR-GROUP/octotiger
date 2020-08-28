881941b988ce3abb46454df6ede459aafd6a1242
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-08-28 02:11:02 +0200

    Added experimental kokkos rho multipole kernel

--- octotiger/multipole_interactions/kernel/kokkos_kernel.hpp
+++ octotiger/multipole_interactions/kernel/kokkos_kernel.hpp
@@ -84,9 +84,103 @@ void multipole_kernel_rho_impl(hpx::kokkos::executor<kokkos_backend_t>& executor
     Kokkos::MDRangePolicy<decltype(executor.instance()), Kokkos::Rank<3>> policy_1(
         executor.instance(), {0, 0, 0}, {INX, INX, INX});
 
+    // TODO (daissgr) Which of the two lambdas to take?
+
+    // Kokkos::parallel_for("kernel multipole non-rho", policy_1,
+    //     [monopoles, centers_of_mass, multipoles, potential_expansions, angular_corrections, theta,
+    //         masks, indicators] CUDA_GLOBAL_METHOD(int idx, int idy, int idz) {
     Kokkos::parallel_for("kernel multipole non-rho", policy_1,
-        [monopoles, centers_of_mass, multipoles, potential_expansions, angular_corrections, theta,
-            masks, indicators] CUDA_GLOBAL_METHOD(int idx, int idy, int idz) {});
+        KOKKOS_LAMBDA(int idx, int idy, int idz) {
+            const size_t component_length = ENTRIES + SOA_PADDING;
+            const size_t component_length_unpadded = INNER_CELLS + SOA_PADDING;
+
+            // Set cell indices
+            const octotiger::fmm::multiindex<> cell_index(idx + INNER_CELLS_PADDING_DEPTH,
+                idy + INNER_CELLS_PADDING_DEPTH, idz + INNER_CELLS_PADDING_DEPTH);
+            octotiger::fmm::multiindex<> cell_index_coarse(cell_index);
+            cell_index_coarse.transform_coarse();
+            const size_t cell_flat_index = octotiger::fmm::to_flat_index_padded(cell_index);
+            octotiger::fmm::multiindex<> cell_index_unpadded(idx, idy, idz);
+            const size_t cell_flat_index_unpadded =
+                octotiger::fmm::to_inner_flat_index_not_padded(cell_index_unpadded);
+
+            // Load multipoles for this cell
+            double m_cell[20];
+            for (int i = 0; i < 20; i++)
+                m_cell[i] = multipoles[i * component_length + cell_flat_index];
+            double X[NDIM];
+            X[0] = centers_of_mass[cell_flat_index];
+            X[1] = centers_of_mass[1 * component_length + cell_flat_index];
+            X[2] = centers_of_mass[2 * component_length + cell_flat_index];
+
+            // Create and set result arrays
+            double tmpstore[20];
+            double tmp_corrections[3];
+            for (size_t i = 0; i < 20; ++i)
+                tmpstore[i] = 0.0;
+            for (size_t i = 0; i < 3; ++i)
+                tmp_corrections[i] = 0.0;
+            // Required for mask
+            const double theta_rec_squared = sqr(1.0 / theta);
+            double m_partner[20];
+            double Y[NDIM];
+
+            // calculate interactions between this cell and each stencil element
+            for (int stencil_x = STENCIL_MIN; stencil_x <= STENCIL_MAX; stencil_x++) {
+                int x = stencil_x - STENCIL_MIN;
+                for (int stencil_y = STENCIL_MIN; stencil_y <= STENCIL_MAX; stencil_y++) {
+                    int y = stencil_y - STENCIL_MIN;
+                    for (int stencil_z = STENCIL_MIN; stencil_z <= STENCIL_MAX; stencil_z++) {
+                        const size_t index = x * STENCIL_INX * STENCIL_INX + y * STENCIL_INX +
+                            (stencil_z - STENCIL_MIN);
+                        if (!masks[index]) {
+                            continue;
+                        }
+                        const double mask_phase_one = indicators[index];
+
+                        const multiindex<> partner_index(cell_index.x + stencil_x,
+                            cell_index.y + stencil_y, cell_index.z + stencil_z);
+                        const size_t partner_flat_index = to_flat_index_padded(partner_index);
+                        multiindex<> partner_index_coarse(partner_index);
+                        partner_index_coarse.transform_coarse();
+                        const double theta_c_rec_squared = static_cast<double>(
+                            distance_squared_reciprocal(cell_index_coarse, partner_index_coarse));
+                        const bool mask_b = theta_rec_squared > theta_c_rec_squared;
+                        double mask = mask_b ? 1.0 : 0.0;
+
+                        // Load data of interaction partner
+                        Y[0] = centers_of_mass[partner_flat_index];
+                        Y[1] = centers_of_mass[1 * component_length + partner_flat_index];
+                        Y[2] = centers_of_mass[2 * component_length + partner_flat_index];
+                        m_partner[0] = monopoles[partner_flat_index] * mask;
+                        mask = mask * mask_phase_one;
+                        m_partner[0] += multipoles[partner_flat_index] * mask;
+                        for (size_t i = 1; i < 20; ++i) {
+                            m_partner[i] =
+                                multipoles[i * component_length + partner_flat_index] * mask;
+                        }
+
+                        // Do the actual calculations
+                        octotiger::fmm::multipole_interactions::compute_kernel_rho(X, Y, m_partner, tmpstore, tmp_corrections, m_cell,
+                            [] (const double& one, const double& two) -> double {
+                                return max(one, two);
+                            });
+                    }
+                }
+            }
+
+            // Store results in output arrays
+            for (size_t i = 0; i < 20; ++i) {
+                potential_expansions[i * component_length_unpadded + cell_flat_index_unpadded] =
+                    tmpstore[i];
+            }
+
+            angular_corrections[cell_flat_index_unpadded] = tmp_corrections[0];
+            angular_corrections[1 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmp_corrections[1];
+            angular_corrections[2 * component_length_unpadded + cell_flat_index_unpadded] =
+                tmp_corrections[2];
+        });
 }
 // --------------------------------------- Kernel non rho implementations
 
@@ -115,8 +209,8 @@ void multipole_kernel_non_rho_impl(hpx::kokkos::executor<kokkos_backend_t>& exec
     // Kokkos::parallel_for("kernel multipole non-rho", policy_1,
     //     [monopoles, centers_of_mass, multipoles, potential_expansions, theta, masks,
     //         indicators] CUDA_GLOBAL_METHOD(int idx, int idy, int idz) {
-    Kokkos::parallel_for("kernel multipole non-rho", policy_1,
-        KOKKOS_LAMBDA(int idx, int idy, int idz) {
+    Kokkos::parallel_for(
+        "kernel multipole non-rho", policy_1, KOKKOS_LAMBDA(int idx, int idy, int idz) {
             const size_t component_length = ENTRIES + SOA_PADDING;
             const size_t component_length_unpadded = INNER_CELLS + SOA_PADDING;
             // Set cell indices
@@ -184,7 +278,7 @@ void multipole_kernel_non_rho_impl(hpx::kokkos::executor<kokkos_backend_t>& exec
                         // Do the actual calculations
                         octotiger::fmm::multipole_interactions::compute_kernel_non_rho(X, Y,
                             m_partner, tmpstore,
-                            [] (const double& one, const double& two) -> double {
+                            [](const double& one, const double& two) -> double {
                                 return max(one, two);
                             });
                     }
