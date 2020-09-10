38ac3008c8d18fcd7f34000940aa1d1dbc5792ac
Parsa Amini <me@parsaamini.net>
2020-09-10 08:22:43 -0500

    exit(1) instead of assert

--- tools/size_checker/size_checker.py
+++ tools/size_checker/size_checker.py
@@ -1,30 +1,36 @@
 #!/usr/bin/env python3
 
 import argparse
+import sys
 from os import listdir
 from os import path
 
 
+def fail_if_not(expected, message):
+    if not expected:
+        print(message, file=sys.stderr)
+        sys.exit(1)
+
+
 def fail_if_file_sizes_differ(original, target):
-    assert path.exists(original), '"' + original + \
-        '" does not exist. Check failed.'
-    assert path.isfile(original), '"' + original + \
-        '" is not a file. Check failed.'
+    fail_if_not(path.exists(original), '"' + original +
+                '" does not exist.')
+    fail_if_not(path.isfile(original), '"' + original +
+                '" is not a file.')
 
-    assert path.exists(target), target + ' does not exist. Check failed.'
-    assert path.isfile(target), target + ' is not a file. Check failed.'
+    fail_if_not(path.exists(target), '"{}" does not exist.'.format(target))
+    fail_if_not(path.isfile(target), '"{}" is not a file.'.format(target))
 
-    assert path.getsize(target) == path.getsize(original), \
-        'File sizes differ between "{}" and "{}". Check failed.'.format(target, original)
+    fail_if_not(path.getsize(target) == path.getsize(original),
+                'File sizes differ between "{}" and "{}".'.format(target, original))
 
 
 def main():
     parser = argparse.ArgumentParser(
         description='Check generated Silo file size(s) against original')
-    parser.add_argument('original',
-                        help='Original Silo file to compare against')
-    parser.add_argument('target',
-                        help='Generated Silo file')
+    parser.add_argument(
+        'original', help='Original Silo file to compare against')
+    parser.add_argument('target', help='Generated Silo file')
 
     args = parser.parse_args()
 
@@ -38,15 +44,16 @@ def main():
     tfn = path.basename(args.target) + '.data'
     tfp = path.join(path.dirname(args.target), tfn)
     if path.isdir(ofp):
-        assert path.isdir(tfp), 'Target silo data directory does not exist. Check failed.'
+        fail_if_not(path.isdir(tfp),
+                    'Target silo data directory does not exist.')
 
         ossf = [path.join(ofp, f) for f in listdir(ofp) if path.isfile(
             path.join(ofp, f)) and path.splitext(path.join(ofp, f))[1] == '.silo']
         tssf = [path.join(tfp, f) for f in listdir(tfp) if path.isfile(
             path.join(tfp, f)) and path.splitext(path.join(tfp, f))[1] == '.silo']
 
-        assert len(ossf) == len(tssf), \
-            'Target silo data has a different number of Silo files. Check failed.'
+        fail_if_not(len(ossf) == len(tssf),
+                    'Target silo data has a different number of Silo files.')
 
         for o, t in zip(ossf, tssf):
             fail_if_file_sizes_differ(o, t)
