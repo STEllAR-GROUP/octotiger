7de55bc388d40f5967464a246b1284401615bf95
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2020-10-07 12:35:56 +0200

    Fixed flux bug

--- src/unitiger/hydro_impl/flux_cuda_kernel.cu
+++ src/unitiger/hydro_impl/flux_cuda_kernel.cu
@@ -11,6 +11,10 @@
 
 #include <hpx/synchronization/once.hpp>
 
+// TODO Duplicated from cell_geometry class - come up with a better way to get this to the device
+// Includes flip_dim, faces, xloc and quad_weights
+// Move cell geometry class to device?
+
 __device__ inline int flip_dim(const int d, const int flip_dim) {
 		int dims[3];
 		int k = d;
@@ -52,6 +56,7 @@ __host__ void init_gpu_masks(bool *masks) {
 }
 
 __host__ const bool* get_gpu_masks(void) {
+    // TODO Create class to handle these read-only, created-once GPU buffers for masks. This is a reoccuring problem
     static bool *masks = recycler::recycle_allocator_cuda_device<bool>{}.allocate(NDIM * 1000);
     hpx::lcos::local::call_once(flag1, init_gpu_masks, masks);
     return masks;
@@ -63,12 +68,12 @@ __device__ const int face_offset = 27 * 1000;
 __device__ const int dim_offset = 1000;
 
 __global__ void
-__launch_bounds__(896, 1)
+__launch_bounds__(128, 2)
  flux_cuda_kernel(const double * __restrict__ q_combined, const double * __restrict__ x_combined, double * __restrict__ f_combined,
     double * amax, int * amax_indices, int * amax_d, const bool * __restrict__ masks, const double omega, const double dx, const double A_, const double B_, const double fgamma, const double de_switch_1) {
-  __shared__ double sm_amax[896];
-  __shared__ int sm_d[896];
-  __shared__ int sm_i[896];
+  __shared__ double sm_amax[128];
+  __shared__ int sm_d[128];
+  __shared__ int sm_i[128];
 
   const int nf = 15;
 
@@ -81,87 +86,77 @@ __launch_bounds__(896, 1)
 
   // 3 dim 1000 i workitems
   const int dim = blockIdx.z;
-  const int index = threadIdx.x * 64 + threadIdx.y * 8 + threadIdx.z + 104;
-  int tid = index - 104;   
+  const int tid = threadIdx.x * 64 + threadIdx.y * 8 + threadIdx.z;
+  const int index = blockIdx.y * 128 + tid + 104;
   for (int f = 0; f < nf; f++) {
       f_combined[dim * 15 * 1000 + f * 1000 + index] = 0.0;
   }
   if (index < 1000) {
-  double mask = masks[index + dim * dim_offset];
-  //if(tid == 0)
-  // printf("starting...");
-  if(mask != 0.0) {
-    for (int fi = 0; fi < 9; fi++) {    // 9
-      double this_ap = 0.0, this_am = 0.0;    // tmps
-      const int d = faces[dim][fi];
-      const int flipped_dim = flip_dim(d, dim);
-      for (int dim = 0; dim < 3; dim++) {
-          local_x[dim] = x_combined[dim * 1000 + index] + (0.5 * xloc[d][dim] * dx);
+    double mask = masks[index + dim * dim_offset];
+    if(mask != 0.0) {
+      for (int fi = 0; fi < 9; fi++) {    // 9
+        double this_ap = 0.0, this_am = 0.0;    // tmps
+        const int d = faces[dim][fi];
+        const int flipped_dim = flip_dim(d, dim);
+        for (int dim = 0; dim < 3; dim++) {
+            local_x[dim] = x_combined[dim * 1000 + index] + (0.5 * xloc[d][dim] * dx);
+        }
+        local_vg[0] = -omega * (x_combined[1000 + index] + 0.5 * xloc[d][1] * dx);
+        local_vg[1] = +omega * (x_combined[index] + 0.5 * xloc[d][0] * dx);
+        local_vg[2] = 0.0;
+        inner_flux_loop2<double>(omega, nf, A_, B_, q_combined, local_f, local_x, local_vg,
+          this_ap, this_am, dim, d, dx, fgamma, de_switch_1,
+          dim_offset * d + index, dim_offset * flipped_dim - compressedH_DN[dim] + index, face_offset);
+        this_ap *= mask;
+        this_am *= mask;
+        const double amax_tmp = max_wrapper(this_ap, (-this_am));
+        if (amax_tmp > current_amax) {
+          current_amax = amax_tmp;
+          current_d = d;
+        }
+        for (int f = 1; f < nf; f++) {
+          f_combined[dim * 15 * 1000 + f * 1000 + index] += quad_weights[fi] * local_f[f];
+        }
       }
-      local_vg[0] = -omega * (x_combined[1000 + index] + 0.5 * xloc[d][1] * dx);
-      local_vg[1] = +omega * (x_combined[index] + 0.5 * xloc[d][0] * dx);
-      local_vg[2] = 0.0;
-      inner_flux_loop2<double>(omega, nf, A_, B_, q_combined, local_f, local_x, local_vg,
-        this_ap, this_am, dim, d, dx, fgamma, de_switch_1, dim_offset * d + index, dim_offset * flipped_dim - compressedH_DN[dim] + index, face_offset);
-      this_ap *= mask;
-      this_am *= mask;
-      const double amax_tmp = max_wrapper(this_ap, (-this_am));
-      if (amax_tmp > current_amax) {
-        current_amax = amax_tmp;
-        current_d = d;
+    }
+    for (int f = 10; f < nf; f++) {
+      f_combined[dim * 15 * 1000 + index] += f_combined[dim * 15 * 1000 + f * 1000 + index];
+    }
+  }
+  // Find maximum:
+  sm_amax[tid] = current_amax;
+  sm_d[tid] = current_d;
+  sm_i[tid] = index;
+  __syncthreads();
+  // Max reduction with multiple warps
+  for (int tid_border = 64; tid_border >= 32; tid_border /= 2) {
+    if(tid < tid_border) {
+      if (sm_amax[tid + tid_border] > sm_amax[tid]) {
+        sm_amax[tid] = sm_amax[tid + tid_border];
+        sm_d[tid] = sm_d[tid + tid_border];
+        sm_i[tid] = sm_i[tid + tid_border];
       }
-      for (int f = 1; f < nf; f++) {
-        f_combined[dim * 15 * 1000 + f * 1000 + index] += quad_weights[fi] * local_f[f];
+    }
+    __syncthreads();
+  }
+  // Max reduction within one warps
+  for (int tid_border = 16; tid_border >= 1; tid_border /= 2) {
+  if(tid < tid_border) {
+      if (sm_amax[tid + tid_border] > sm_amax[tid]) {
+        sm_amax[tid] = sm_amax[tid + tid_border];
+        sm_d[tid] = sm_d[tid + tid_border];
+        sm_i[tid] = sm_i[tid + tid_border];
       }
-   }
- }
- for (int f = 10; f < nf; f++) {
-   f_combined[dim * 15 * 1000 + index] += f_combined[dim * 15 * 1000 + f * 1000 + index];
- }
+    }
   }
 
- // Find maximum:
- sm_amax[tid] = current_amax;
- sm_d[tid] = current_d;
- sm_i[tid] = tid + 100;
- __syncthreads();
- // First step as we do not have multiples of 2
- if(tid < 448) {
-   if (sm_amax[tid + 448 ] > sm_amax[tid]) {
-     sm_amax[tid] = sm_amax[tid + 448];
-     sm_d[tid] = sm_d[tid + 448];
-     sm_i[tid] = sm_i[tid + 448];
-   }
- }
- __syncthreads();
- // Max reduction with multiple warps
- for (int tid_border = 256; tid_border >= 32; tid_border /= 2) {
-   if(tid < tid_border) {
-     if (sm_amax[tid + tid_border] > sm_amax[tid]) {
-       sm_amax[tid] = sm_amax[tid + tid_border];
-       sm_d[tid] = sm_d[tid + tid_border];
-       sm_i[tid] = sm_i[tid + tid_border];
-     }
-   }
-   __syncthreads();
- }
- // Max reduction within one warps
- for (int tid_border = 16; tid_border >= 1; tid_border /= 2) {
-   if(tid < tid_border) {
-     if (sm_amax[tid + tid_border] > sm_amax[tid]) {
-       sm_amax[tid] = sm_amax[tid + tid_border];
-       sm_d[tid] = sm_d[tid + tid_border];
-       sm_i[tid] = sm_i[tid + tid_border];
-     }
-   }
- }
-
- if (tid == 0) {
-   amax[dim] = sm_amax[0];
-   amax_indices[dim] = sm_i[0];
-   amax_d[dim] = sm_d[0];
- }
- return;
+  if (tid == 0) {
+   //printf("Block %i %i TID %i %i \n", blockIdx.y, blockIdx.z, tid, index);
+   amax[blockIdx.y + dim * 7] = sm_amax[0];
+   amax_indices[blockIdx.y + dim * 7] = sm_i[0];
+   amax_d[blockIdx.y + dim * 7] = sm_d[0];
+  }
+  return;
 }
 
 timestep_t launch_flux_cuda(stream_interface<hpx::cuda::experimental::cuda_executor, pool_strategy>& executor,
@@ -172,62 +167,42 @@ timestep_t launch_flux_cuda(stream_interface<hpx::cuda::experimental::cuda_execu
     timestep_t ts;
     const cell_geometry<3, 8> geo;
 
-
-    /*size_t device_id =
-      stream_pool::get_next_device_id<hpx::cuda::experimental::cuda_executor,
-      pool_strategy>();*/
-    //size_t device_id = 0;
-
-    //stream_interface<hpx::cuda::experimental::cuda_executor, pool_strategy> executor;
-
-    /*recycler::cuda_device_buffer<double> device_q(15 * 27 * 10 * 10 * 10 + 32, device_id);
-    hpx::apply(static_cast<hpx::cuda::experimental::cuda_executor>(executor),
-    cudamemcpyasync, device_q.device_side_buffer,
-    combined_q.data(), (15 * 27 * 10 * 10 * 10 + 32) * sizeof(double), cudamemcpyhosttodevice);*/
-
-    /*std::vector<double, recycler::recycle_allocator_cuda_host<double>> combined_x(NDIM * 1000 + 32);
-    convert_x_structure(X, combined_x);
-    double dx = X[0][geo.H_DNX] - X[0][0];
-    recycler::cuda_device_buffer<double> device_x(NDIM * 1000 + 32, device_id);
-    hpx::apply(static_cast<hpx::cuda::experimental::cuda_executor>(executor),
-    cudaMemcpyAsync, device_x.device_side_buffer,
-    combined_x.data(), (NDIM * 1000 + 32) * sizeof(double), cudaMemcpyHostToDevice);*/
-
     recycler::cuda_device_buffer<double> device_f(NDIM * 15 * 1000 + 32, device_id);
     const bool *masks = get_gpu_masks();
 
-    recycler::cuda_device_buffer<double> device_amax(NDIM);
-    recycler::cuda_device_buffer<int> device_amax_indices(NDIM);
-    recycler::cuda_device_buffer<int> device_amax_d(NDIM);
+    recycler::cuda_device_buffer<double> device_amax(7 * NDIM);
+    recycler::cuda_device_buffer<int> device_amax_indices(7 * NDIM);
+    recycler::cuda_device_buffer<int> device_amax_d(7 * NDIM);
     double A_ = physics<NDIM>::A_;
     double B_ = physics<NDIM>::B_;
     double fgamma = physics<NDIM>::fgamma_;
     double de_switch_1 = physics<NDIM>::de_switch_1;
 
-    dim3 const grid_spec(1, 1, 3);
-    dim3 const threads_per_block(14, 8, 8);
+    dim3 const grid_spec(1, 7, 3);
+    dim3 const threads_per_block(2, 8, 8);
     void* args[] = {&(device_q),
       &(device_x), &(device_f.device_side_buffer), &(device_amax.device_side_buffer),
-      &(device_amax_indices.device_side_buffer), &(device_amax_d.device_side_buffer), &masks, &omega, &dx, &A_, &B_, &fgamma, &de_switch_1};
+      &(device_amax_indices.device_side_buffer), &(device_amax_d.device_side_buffer),
+      &masks, &omega, &dx, &A_, &B_, &fgamma, &de_switch_1};
     executor.post(
     cudaLaunchKernel<decltype(flux_cuda_kernel)>,
     flux_cuda_kernel, grid_spec, threads_per_block, args, 0);
 
     // Move data to host
-    std::vector<double, recycler::recycle_allocator_cuda_host<double>> amax(NDIM);
-    std::vector<int, recycler::recycle_allocator_cuda_host<int>> amax_indices(NDIM);
-    std::vector<int, recycler::recycle_allocator_cuda_host<int>> amax_d(NDIM);
+    std::vector<double, recycler::recycle_allocator_cuda_host<double>> amax(7 * NDIM);
+    std::vector<int, recycler::recycle_allocator_cuda_host<int>> amax_indices(7 * NDIM);
+    std::vector<int, recycler::recycle_allocator_cuda_host<int>> amax_d(7 * NDIM);
     hpx::apply(static_cast<hpx::cuda::experimental::cuda_executor>(executor),
                cudaMemcpyAsync, amax.data(),
-               device_amax.device_side_buffer, NDIM * sizeof(double),
+               device_amax.device_side_buffer, 7 * NDIM * sizeof(double),
                cudaMemcpyDeviceToHost);
     hpx::apply(static_cast<hpx::cuda::experimental::cuda_executor>(executor),
                cudaMemcpyAsync, amax_indices.data(),
-               device_amax_indices.device_side_buffer, NDIM * sizeof(int),
+               device_amax_indices.device_side_buffer, 7 * NDIM * sizeof(int),
                cudaMemcpyDeviceToHost);
     hpx::apply(static_cast<hpx::cuda::experimental::cuda_executor>(executor),
                cudaMemcpyAsync, amax_d.data(),
-               device_amax_d.device_side_buffer, NDIM * sizeof(int),
+               device_amax_d.device_side_buffer, 7 * NDIM * sizeof(int),
                cudaMemcpyDeviceToHost);
     auto fut = hpx::async(static_cast<hpx::cuda::experimental::cuda_executor>(executor),
                cudaMemcpyAsync, combined_f.data(), device_f.device_side_buffer,
@@ -236,12 +211,11 @@ timestep_t launch_flux_cuda(stream_interface<hpx::cuda::experimental::cuda_execu
 
     // Find Maximum
     size_t current_dim = 0;
-    for (size_t dim_i = 1; dim_i < NDIM; dim_i++) {
+    for (size_t dim_i = 1; dim_i < 7 * NDIM; dim_i++) {
       if (amax[dim_i] > amax[current_dim]) { 
         current_dim = dim_i;
       }
     }
-    //std::cin.get();
     std::vector<double> URs(nf_), ULs(nf_);
     const size_t current_max_index = amax_indices[current_dim];
     const size_t current_d = amax_d[current_dim];
@@ -249,6 +223,7 @@ timestep_t launch_flux_cuda(stream_interface<hpx::cuda::experimental::cuda_execu
     ts.x = combined_x[current_max_index];
     ts.y = combined_x[current_max_index + 1000];
     ts.z = combined_x[current_max_index + 2000];
+    current_dim = current_dim / 7;
     const auto flipped_dim = geo.flip_dim(current_d, current_dim);
     constexpr int compressedH_DN[3] = {100, 10, 1};
     for (int f = 0; f < nf_; f++) {
