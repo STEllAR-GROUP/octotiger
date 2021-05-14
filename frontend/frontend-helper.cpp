e799729f37af1c6af12beb722e670ee41a4e44e4
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-05-14 10:54:24 +0200

    Added more outputs

--- frontend/frontend-helper.cpp
+++ frontend/frontend-helper.cpp
@@ -31,11 +31,11 @@
 
 #endif
 #include "octotiger/common_kernel/interaction_constants.hpp"
-#include "octotiger/monopole_interactions/util/calculate_stencil.hpp"
-#include "octotiger/monopole_interactions/legacy/p2m_interaction_interface.hpp"
 #include "octotiger/monopole_interactions/legacy/monopole_interaction_interface.hpp"
-#include "octotiger/multipole_interactions/util/calculate_stencil.hpp"
+#include "octotiger/monopole_interactions/legacy/p2m_interaction_interface.hpp"
+#include "octotiger/monopole_interactions/util/calculate_stencil.hpp"
 #include "octotiger/multipole_interactions/legacy/multipole_interaction_interface.hpp"
+#include "octotiger/multipole_interactions/util/calculate_stencil.hpp"
 
 #include <hpx/collectives/broadcast_direct.hpp>
 #include <hpx/hpx_init.hpp>
@@ -56,39 +56,6 @@
 #else
 #include <cfloat>
 #endif
-
-std::array<size_t, 7> sum_counters_worker(std::size_t desired) {
-    std::array<size_t, 7> ret;
-    ret[0] = std::size_t(-1);
-    ret[1] = 0;
-    ret[2] = 0;
-    ret[3] = 0;
-    ret[4] = 0;
-    ret[5] = 0;
-    ret[6] = 0;
-    std::size_t current = hpx::get_worker_thread_num();
-    if (current == desired) {
-        using cuda_multi_intfc =
-            octotiger::fmm::multipole_interactions::multipole_interaction_interface;
-        using cuda_mono_intfc = octotiger::fmm::monopole_interactions::monopole_interaction_interface;
-
-        ret[0] = desired;
-        ret[1] = cuda_multi_intfc::cpu_launch_counter();
-        ret[2] = cuda_multi_intfc::cuda_launch_counter();
-
-        ret[3] = cuda_mono_intfc::cpu_launch_counter();
-        ret[4] = cuda_mono_intfc::cuda_launch_counter();
-
-        ret[5] = cuda_multi_intfc::cpu_launch_counter_non_rho();
-        ret[6] = cuda_multi_intfc::cuda_launch_counter_non_rho();
-
-        // std::cout << "OS-thread " << ret[1] << " "
-        //           << ret[2] << " " << ret[3]
-        //           << " " << ret[4] << std::endl;
-    }
-    return ret;
-}
-HPX_PLAIN_ACTION(sum_counters_worker, sum_counters_worker_action);
 
 void initialize(options _opts, std::vector<hpx::id_type> const& localities) {
     std::cerr << "Started initialize method" << std::endl;
@@ -125,7 +92,7 @@ void initialize(options _opts, std::vector<hpx::id_type> const& localities) {
 
 void cleanup_buffers() {
     cleanup_puddle_on_this_locality();
-    return ;
+    return;
 }
 HPX_PLAIN_ACTION(cleanup_buffers, cleanup_buffers_action);
 
@@ -139,9 +106,8 @@ void cleanup() {
         using action_type = cleanup_buffers_action;
         futures.push_back(hpx::async<action_type>(node));
     }
-	auto f = hpx::when_all(futures.begin(), futures.end());
+    auto f = hpx::when_all(futures.begin(), futures.end());
     f.get();
-    std::cout << "Localities cleanup finished" << std::endl;
 }
 
 HPX_PLAIN_ACTION(initialize, initialize_action);
@@ -156,53 +122,69 @@ void start_octotiger(int argc, char* argv[]) {
             std::cerr << "Finished processing options" << std::endl;
             auto all_locs = hpx::find_all_localities();
             hpx::lcos::broadcast<initialize_action>(all_locs, opts(), all_locs).get();
+            std::cerr << "Finished init" << std::endl;
 
             hpx::id_type root_id = hpx::new_<node_server>(hpx::find_here()).get();
             node_client root_client(root_id);
             node_server* root = root_client.get_ptr().get();
+            std::cerr << "Found root" << std::endl;
 
             node_count_type ngrids;
             //		printf("1\n");
             if (!opts().restart_filename.empty()) {
-                std::cout << "Loading from " << opts().restart_filename << " ...\n";
+                std::cerr << "Loading from " << opts().restart_filename << " ...\n";
                 load_data_from_silo(opts().restart_filename, root, root_client.get_unmanaged_gid());
-                printf("Re-grid\n");
+                std::cerr << "Re-grid" << std::endl;
                 ngrids = root->regrid(root_client.get_unmanaged_gid(), ZERO, -1, true, false);
-                printf("Done. \n");
+                std::cerr << "Done!" << std::endl;
 
                 set_AB(physcon().A, physcon().B);
 
             } else {
+                std::cerr << "Starting refinement" << std::endl;
                 for (integer l = 0; l < opts().max_level; ++l) {
                     ngrids =
                         root->regrid(root_client.get_gid(), grid::get_omega(), -1, false, false);
-                    printf("---------------Created Level %i---------------\n\n", int(l + 1));
+                    std::cerr << "---------------Created Level " << int(l + 1)
+                              << "---------------\n"
+                              << std::endl;
                 }
                 ngrids = root->regrid(root_client.get_gid(), grid::get_omega(), -1, false, false);
-                printf(
-                    "---------------Re-gridded Level %i---------------\n\n", int(opts().max_level));
+                std::cerr << "---------------Re-gridded Level " << int(opts().max_level)
+                          << "---------------\n"
+                          << std::endl;
             }
             for (integer l = 0; l < opts().extra_regrid; ++l) {
+                std::cerr << "Starting extra regridding step..." << std::endl;
                 ngrids = root->regrid(root_client.get_gid(), grid::get_omega(), -1, false, false);
+                std::cerr << "Finished extra regridding step..." << std::endl;
             }
 
             if (opts().gravity && opts().stop_step != 0) {
-                printf("solving gravity------------\n");
+                std::cerr << "solving gravity------------" << std::endl;
                 root->solve_gravity(false, false);
-                printf("...done\n");
+                std::cerr << "...done" << std::endl;
             }
             if (opts().problem != AMR_TEST) {
+                std::cerr << "Start execution the solver..." << std::endl;
                 hpx::async(&node_server::execute_solver, root,
                     opts().problem == DWD && opts().restart_filename.empty(), ngrids)
                     .get();
+                std::cerr << "Finished solver exeuction - Scenario done!" << std::endl;
             } else {
+                std::cerr << "Start AMR test..." << std::endl;
                 root->enforce_bc();
                 auto e = root->amr_error();
+                std::cerr << "Finished AMR test" << std::endl;
                 printf("AMR Error: %e %e %e\n", e.first, e.second, e.first / e.second);
                 output_all(root, "X", 0, true);
             }
+            std::cerr << "Start timings report..." << std::endl;
             root->report_timing();
-            cleanup(); // cleanup buffer and executor pools
+            std::cerr << "Finished timings report!" << std::endl;
+            std::cerr << "Start cleanup..." << std::endl;
+            cleanup();    // cleanup buffer and executor pools
+            std::cerr << "Localities cleanup finished" << std::endl;
         }
     } catch (...) {
         throw;
