b006e1db8f7595f2bcafd9697d34c721a88c9051
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-06-23 13:47:53 +0200

    Try running basic tests

--- .jenkins/lsu/entry.sh
+++ .jenkins/lsu/entry.sh
@@ -3,4 +3,5 @@
 set -eux
 
 pwd
-printenv
+srun -p QxV100 -N 1 -n 1 -t 00:20:00 bash -c 'module load hwloc cuda; ./build-all.sh ${build_type} ${compiler_config} with-cuda without-mpi without-papi without-apex with-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger' 
+srun -p QxV100 -N 1 -n 1 -t 00:20:00 bash -c 'module load hwloc cuda; cd build/octotiger/build;ctest'
