4f0c29ec7bebe8fc91685052b428a9f6fca21cc2
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-06-24 01:13:59 +0200

    Run sphere tests

--- .jenkins/lsu/entry.sh
+++ .jenkins/lsu/entry.sh
@@ -3,4 +3,5 @@
 set -eux
 
 pwd
-srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load hwloc cuda && ./build-all.sh Release with-CC with-cuda without-mpi without-papi without-apex without-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx cppuddle octotiger && cd build/octotiger/build && ctest -R star --verbose' 
+module hwloc load cuda/11.0
+srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load hwloc cuda/11.0 && ./build-all.sh Release with-CC with-cuda without-mpi without-papi without-apex without-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx cppuddle octotiger && cd build/octotiger/build && ctest -R sphere --verbose' 
