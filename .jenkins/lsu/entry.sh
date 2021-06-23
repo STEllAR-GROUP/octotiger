1c26876f36e1ebb6ddd53f1a01ca731cff9cc9b2
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-06-24 01:34:59 +0200

    Switch to proper compiler

--- .jenkins/lsu/entry.sh
+++ .jenkins/lsu/entry.sh
@@ -3,5 +3,5 @@
 set -eux
 
 pwd
-module load hwloc cuda/11.0
-srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load hwloc cuda/11.0 && ./build-all.sh Release with-CC with-cuda without-mpi without-papi without-apex without-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx cppuddle octotiger && cd build/octotiger/build && ctest -R sphere --verbose' 
+module load gcc/9.3.0 cuda/11.0 hwloc
+srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load gcc/9.3.0 cuda/11.0 hwloc && ./build-all.sh Release with-CC with-cuda without-mpi without-papi without-apex without-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx cppuddle octotiger && cd build/octotiger/build && ctest -R sphere --verbose' 
