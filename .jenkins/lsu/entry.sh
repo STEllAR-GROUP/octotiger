f0877c59c99ca0fbe23e28522b9694927d495a9d
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-06-23 16:19:58 +0200

    Remove kokkos for testing

--- .jenkins/lsu/entry.sh
+++ .jenkins/lsu/entry.sh
@@ -3,4 +3,4 @@
 set -eux
 
 pwd
-srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load hwloc cuda && ./build-all.sh Release with-CC with-cuda without-mpi without-papi without-apex without-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest -R star' 
+srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load hwloc cuda && ./build-all.sh Release with-CC with-cuda without-mpi without-papi without-apex without-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx cppuddle octotiger && cd build/octotiger/build && ctest -R star' 
