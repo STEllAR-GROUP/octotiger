5fd6d7320ca98052d8d58f9a8c0fcf879dbcbc8e
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-06-24 02:13:27 +0200

    Build kokkos

--- .jenkins/lsu/entry.sh
+++ .jenkins/lsu/entry.sh
@@ -4,4 +4,4 @@ set -eux
 
 pwd
 module load gcc/9.3.0 cuda/11.0 hwloc
-srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load gcc/9.3.0 cuda/11.0 hwloc && ./build-all.sh Release with-CC with-cuda without-mpi without-papi without-apex with-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx cppuddle octotiger && cd build/octotiger/build && ctest ' 
+srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load gcc/9.3.0 cuda/11.0 hwloc && ./build-all.sh Release with-CC with-cuda without-mpi without-papi without-apex with-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest ' 
