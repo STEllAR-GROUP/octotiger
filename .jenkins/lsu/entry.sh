dda8ccde4ef922dbbfa8c4368934802982a16159
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-06-25 13:24:59 +0200

    Try setting build status

--- .jenkins/lsu/entry.sh
+++ .jenkins/lsu/entry.sh
@@ -22,7 +22,7 @@ srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load ${compiler_module} cud
 # Tests with griddim = 16
 sed -i 's/GRIDDIM=8/GRIDDIM=16/' build-octotiger.sh
 echo "Running tests with griddim=16"
-srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load ${compiler_module} cuda/11.0 hwloc && ./build-all.sh Release ${compiler_config} with-cuda without-mpi without-papi without-apex with-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling octotiger && cd build/octotiger/build && ctest ' 
+#srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load ${compiler_module} cuda/11.0 hwloc && ./build-all.sh Release ${compiler_config} with-cuda without-mpi without-papi without-apex with-kokkos with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling octotiger && cd build/octotiger/build && ctest ' 
 
 # Reset buildscripts (in case of failure, the next job will reset it in the checkout step)
 sed -i 's/GRIDDIM=16/GRIDDIM=8/' build-octotiger.sh
