92faec64b37deb107fc154e927eb32c36ee8f293
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2021-06-28 14:35:55 +0200

    Re-enable tests

--- .jenkins/lsu/entry.sh
+++ .jenkins/lsu/entry.sh
@@ -18,24 +18,27 @@ module load "${compiler_module}" cuda/11.0 hwloc
 # Tests with griddim = 8
 if [ "${kokkos_config}" = "with-kokkos" ]; then
 	echo "Running tests with griddim=8 on diablo"
-	#srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load ${compiler_module} cuda/11.0 hwloc && ./build-all.sh Release ${compiler_config} ${cuda_config} without-mpi without-papi without-apex ${kokkos_config} with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest ' 
+	srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load ${compiler_module} cuda/11.0 hwloc && ./build-all.sh Release ${compiler_config} ${cuda_config} without-mpi without-papi without-apex ${kokkos_config} with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest ' 
 
-	# Tests with griddim = 16
-	#sed -i 's/GRIDDIM=8/GRIDDIM=16/' build-octotiger.sh
-	#echo "Running tests with griddim=16 on diablo"
-	#srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load ${compiler_module} cuda/11.0 hwloc && ./build-all.sh Release ${compiler_config} ${cuda_config} without-mpi without-papi without-apex ${kokkos_config} with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest ' 
+	# Tests with griddim = 16 - only test in full kokkos + cuda build
+	if [ "${cuda_config}" = "with-cuda" ]; then
+		sed -i 's/GRIDDIM=8/GRIDDIM=16/' build-octotiger.sh
+		echo "Running tests with griddim=16 on diablo"
+		srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load ${compiler_module} cuda/11.0 hwloc && ./build-all.sh Release ${compiler_config} ${cuda_config} without-mpi without-papi without-apex ${kokkos_config} with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest ' 
+		sed -i 's/GRIDDIM=16/GRIDDIM=8/' build-octotiger.sh
+	fi
 else
 	echo "Running tests with griddim=8 on diablo"
 	#srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load ${compiler_module} cuda/11.0 hwloc && ./build-all.sh Release ${compiler_config} ${cuda_config} without-mpi without-papi without-apex ${kokkos_config} with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx cppuddle octotiger && cd build/octotiger/build && ctest ' 
 
 	# Tests with griddim = 16
-	#sed -i 's/GRIDDIM=8/GRIDDIM=16/' build-octotiger.sh
-	#echo "Running tests with griddim=16 on diablo"
-	#srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load ${compiler_module} cuda/11.0 hwloc && ./build-all.sh Release ${compiler_config} ${cuda_config} without-mpi without-papi without-apex ${kokkos_config} with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx cppuddle octotiger && cd build/octotiger/build && ctest ' 
+	# sed -i 's/GRIDDIM=8/GRIDDIM=16/' build-octotiger.sh
+	# echo "Running tests with griddim=16 on diablo"
+	# srun -p QxV100 -N 1 -n 1 -t 01:00:00 bash -c 'module load ${compiler_module} cuda/11.0 hwloc && ./build-all.sh Release ${compiler_config} ${cuda_config} without-mpi without-papi without-apex ${kokkos_config} with-simd with-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx cppuddle octotiger && cd build/octotiger/build && ctest ' 
 fi
 
 # Reset buildscripts (in case of failure, the next job will reset it in the checkout step)
-sed -i 's/GRIDDIM=16/GRIDDIM=8/' build-octotiger.sh
+#sed -i 's/GRIDDIM=16/GRIDDIM=8/' build-octotiger.sh
 if [ "${compiler_config}" = "with-CC-clang" ]; then
   sed -i 's/OCTOTIGER_WITH_BLAST_TEST=OFF/OCTOTIGER_WITH_BLAST_TEST=ON/' build-octotiger.sh
 fi
