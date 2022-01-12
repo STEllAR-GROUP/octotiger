8eea1c96363f9656748631a474e6e00fe6b45419
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2022-01-12 06:56:15 +0100

    Adapt all modules on rostam...

--- .jenkins/lsu/hip-tests-entry.sh
+++ .jenkins/lsu/hip-tests-entry.sh
@@ -3,7 +3,7 @@
 set -eux
 
 # Load everything
-module load gcc/9.3.0 rocm/4.3.0 hwloc
+module load gcc/9.4.0 rocm/4.5.2 hwloc
 
 # Tests with griddim = 8
 srun -p mi100 -N 1 -n 1 -t 01:00:00 bash -c "module load gcc/9.4.0 rocm/4.5.2 hwloc && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest --output-on-failure " 
