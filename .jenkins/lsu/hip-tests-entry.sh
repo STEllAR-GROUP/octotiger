d658ab240a27b2b80dcf7ac68f543b5d7eb70a67
Gregor Daiss <Gregor.Daiss+git@gmail.com>
2022-01-19 15:46:29 -0600

    Only load module on compute node

--- .jenkins/lsu/hip-tests-entry.sh
+++ .jenkins/lsu/hip-tests-entry.sh
@@ -2,9 +2,6 @@
 
 set -eux
 
-# Load everything
-module load rocm/4.3.1 hwloc
-
 # Tests with griddim = 8
 srun -p mi100 -N 1 -n 1 -t 01:00:00 bash -c "module load rocm/4.3.1 hwloc && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest --output-on-failure " 
 
