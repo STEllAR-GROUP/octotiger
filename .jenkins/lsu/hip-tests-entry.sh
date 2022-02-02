5dbc7f343b14730e926e6dd12ffc9741274d1c11
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2022-02-02 06:25:43 +0100

    Add rocminfo output to HIP jenkins pipeline
    Catches re-occuring permission issues on the jenkins node

--- .jenkins/lsu/hip-tests-entry.sh
+++ .jenkins/lsu/hip-tests-entry.sh
@@ -3,10 +3,10 @@
 set -eux
 
 # Tests with griddim = 8
-srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load gcc/9.4.0 rocm/4.3.1 hwloc && module list && hipcc --version && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest --output-on-failure " 
+srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load gcc/9.4.0 rocm/4.3.1 hwloc && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest --output-on-failure " 
 
 # Tests with griddim = 16
 sed -i 's/GRIDDIM=8/GRIDDIM=16/' build-octotiger.sh
-srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load gcc/9.4.0 rocm/4.3.1 hwloc && module list && hipcc --version && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 octotiger && cd build/octotiger/build && ctest --output-on-failure " 
+srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load gcc/9.4.0 rocm/4.3.1 hwloc && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 octotiger && cd build/octotiger/build && ctest --output-on-failure " 
 sed -i 's/GRIDDIM=16/GRIDDIM=8/' build-octotiger.sh
 
