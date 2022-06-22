f21374eb494b2208e87c3e048943f33f723c38ca
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2022-06-22 17:24:48 +0200

    Re-add gcc module

--- .jenkins/lsu/hip-tests-entry.sh
+++ .jenkins/lsu/hip-tests-entry.sh
@@ -3,10 +3,11 @@
 set -eux
 
 # Tests with griddim = 8
-srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load llvm/14 rocm/4.5.2 hwloc && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest --output-on-failure -E legacy " 
+# we need the gcc 10 module for the silo fortran compilation...
+srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load gcc/10 rocm/4.5.2 hwloc && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest --output-on-failure -E legacy " 
 
 # Tests with griddim = 16
 sed -i 's/GRIDDIM=8/GRIDDIM=16/' build-octotiger.sh
-srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load llvm/14 rocm/4.5.2 hwloc && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 octotiger && cd build/octotiger/build && ctest --output-on-failure -E legacy " 
+srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load gcc/10 rocm/4.5.2 hwloc && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 octotiger && cd build/octotiger/build && ctest --output-on-failure -E legacy " 
 sed -i 's/GRIDDIM=16/GRIDDIM=8/' build-octotiger.sh
 
