76a21acd2898d5ae16d05f2f4a52db0e474fbb7e
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2022-06-24 15:58:03 +0200

    Fix hip jenkins tests

--- .jenkins/lsu/hip-tests-entry.sh
+++ .jenkins/lsu/hip-tests-entry.sh
@@ -14,6 +14,6 @@ srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load gcc/12 rocm/5 
 
 # Tests with griddim = 16
 sed -i 's/GRIDDIM=8/GRIDDIM=16/' build-octotiger.sh
-srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load gcc/12 rocm/5 hwloc && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 octotiger && cd build/octotiger/build && ctest --output-on-failure " 
+srun -p jenkins-amdgpu -N 1 -n 1 -t 02:00:00 bash -c "module load gcc/12 rocm/5 hwloc && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 octotiger && cd build/octotiger/build && ctest --output-on-failure -E legacy " 
 sed -i 's/GRIDDIM=16/GRIDDIM=8/' build-octotiger.sh
 
