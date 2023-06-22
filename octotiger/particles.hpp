f7f6a49b74926c1c8c072cac2a987ec69645f7e0
shibersag <shibersag@gmail.com>
2023-06-22 09:11:55 -0500

    Upgrading particle functions

--- octotiger/particles.hpp
+++ octotiger/particles.hpp
@@ -40,9 +40,11 @@ struct particle {
 	space_vector vel;
 	real mass;
 	expansion L; //expansion coefficient
+	space_vector L_c; // AM correction
 	real pot;
 	space_vector g;
 	integer part_type;
+	integer id;
 	std::array<integer, NDIM> containing_cell_index;
 	std::array<integer, NDIM> containing_parent_cell_index;
 	space_vector pos_rel_cell_center;
@@ -67,15 +69,25 @@ struct particle {
 		(*this).part_type = ptype;
 	//	printf("created particle m: %e=%e, pos: (%e,%e,%e)=(%e,%e,%e)\n", (*this).mass, m, (*this).pos[0], (*this).pos[1], (*this).pos[2], x[0], x[1], x[2]);
         }
+        particle(real m, space_vector x, space_vector v, integer pid, integer ptype = 0) {
+                (*this).mass = m;
+                for (integer d = 0; d < NDIM; ++d) {
+                        (*this).pos[d] = x[d];
+                        (*this).vel[d] = v[d];
+                }
+                (*this).part_type = ptype;
+                (*this).id = pid;
+                //printf("created particle m: %e=%e, pos: (%e,%e,%e)=(%e,%e,%e)\n", (*this).mass, m, (*this).pos[0], (*this).pos[1], (*this).pos[2], x[0], x[1], x[2]);        
+        }
 	bool is_in_boundary(space_vector bmin, space_vector bmax) {
 		real xp = (*this).pos[XDIM];
-		if ((bmin[XDIM] < xp) && (xp < bmax[XDIM])) {
+		if ((bmin[XDIM] <= xp) && (xp < bmax[XDIM])) {
 			if (NDIM > 1) {
 				xp = (*this).pos[YDIM];
-				if ((bmin[YDIM] < xp) && (xp < bmax[YDIM])) {
+				if ((bmin[YDIM] <= xp) && (xp < bmax[YDIM])) {
 					if (NDIM > 2) {
 						xp = (*this).pos[ZDIM];
-						if ((bmin[ZDIM] < xp) && (xp < bmax[ZDIM])) {
+						if ((bmin[ZDIM] <= xp) && (xp < bmax[ZDIM])) {
 							return true;
 						}
 					} else {
@@ -105,7 +117,9 @@ struct particle {
                 arc & pos;
                 arc & vel;
                 arc & mass;
+		arc & id;
                 arc & L;
+		arc & L_c;
 		arc & containing_cell_index;
         }
 };
@@ -127,6 +141,7 @@ struct particle_sample {
 OCTOTIGER_EXPORT std::vector<particle> load_particles();
 OCTOTIGER_EXPORT std::vector<particle> load_particles(space_vector bmin, space_vector bmax);
 OCTOTIGER_EXPORT std::vector<particle> load_particles(std::vector<particle> particles, space_vector bmin, space_vector bmax);
+OCTOTIGER_EXPORT std::vector<integer> get_particles_inds(std::vector<particle> particles, integer cell_index);
 
 #ifdef CHECK_BOUNDS
 #define CHECK_PART_BOUNDS(i)                                                                                                                            \
--- src/particles.cpp
+++ src/particles.cpp
@@ -14,7 +14,7 @@ std::vector<particle> load_particles() {
 	                        part_pos[ZDIM] = opts().Part_Z[part_i];
                         }
                 }
-                particles.push_back(particle(opts().Part_M[part_i], part_pos));
+                particles.push_back(particle(opts().Part_M[part_i], part_pos, part_i, 1));
         }
 	return particles;
 }
@@ -23,13 +23,16 @@ std::vector<particle> load_particles(space_vector bmin, space_vector bmax) {
         for (integer part_i = 0; part_i < opts().Part_M.size(); part_i++) {
                 space_vector part_pos;
                 part_pos[XDIM] = opts().Part_X[part_i];
+		part_vel[XDIM] = opts().Part_VX[part_i];
                 if (NDIM > 1) {
                         part_pos[YDIM] = opts().Part_Y[part_i];
+			part_vel[YDIM] = opts().Part_VY[part_i];
                         if (NDIM > 2) {
                                 part_pos[ZDIM] = opts().Part_Z[part_i];
+				part_vel[ZDIM] = opts().Part_VZ[part_i];
                         }
                 }
-                particle p = particle(opts().Part_M[part_i], part_pos); 
+                particle p = particle(opts().Part_M[part_i], part_pos, part_vel, part_i, 1); 
                 if (p.is_in_boundary(bmin, bmax)) { 
                         particles.push_back(p);
                 }
@@ -45,3 +48,16 @@ std::vector<particle> load_particles(std::vector<particle> particles, space_vect
         }
         return particles_in_b;
 }
+std::vector<integer> get_particles_inds(std::vector<particle> particles, integer cell_index) {
+        std::vector<integer> inds;
+        for (integer i = 0; i < particles.size(); i++) {
+                const particle p = particles[i];
+                const auto p_inds = p.containing_cell_index;
+                const integer pindex = gindex(p_inds[XDIM], p_inds[YDIM], p_inds[ZDIM]);
+                if (pindex == cell_index) {
+                        inds.push_back(i);
+                }
+        }
+        return inds;
+}
+
