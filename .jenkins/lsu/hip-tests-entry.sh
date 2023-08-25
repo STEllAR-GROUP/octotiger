e778e14f76bc70a080b627f6faea8c97e5b4be11
Gregor Daiß <Gregor.Daiss+git@gmail.com>
2023-08-25 08:18:48 -0500

    Re-enable 16^3 hip tests excluding only the memory-intensive ones

--- .jenkins/lsu/hip-tests-entry.sh
+++ .jenkins/lsu/hip-tests-entry.sh
@@ -13,7 +13,7 @@ set -eux
 srun -p mi100 -N 1 -n 1 -t 04:00:00 bash -lc "module load rocm/5.4 hwloc llvm/14 && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 boost jemalloc hdf5 silo vc hpx kokkos cppuddle octotiger && cd build/octotiger/build && ctest --output-on-failure " 
 
 # Tests with griddim = 16
-#sed -i 's/GRIDDIM=8/GRIDDIM=16/' build-octotiger.sh
-#srun -p mi100 -N 1 -n 1 -t 04:00:00 bash -lc "module load rocm/5.4 hwloc llvm/14 && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 octotiger && cd build/octotiger/build && ctest --output-on-failure -E legacy " 
-#sed -i 's/GRIDDIM=16/GRIDDIM=8/' build-octotiger.sh
+sed -i 's/GRIDDIM=8/GRIDDIM=16/' build-octotiger.sh
+srun -p mi100 -N 1 -n 1 -t 04:00:00 bash -lc "module load rocm/5.4 hwloc llvm/14 && module list && hipcc --version && rocminfo && ./build-all.sh Release with-CC-clang without-cuda without-mpi without-papi without-apex with-kokkos with-simd without-hpx-backend-multipole without-hpx-backend-monopole with-hpx-cuda-polling without-otf2 octotiger && cd build/octotiger/build && ctest --output-on-failure -E legacy " 
+sed -i 's/GRIDDIM=16/GRIDDIM=8/' build-octotiger.sh
 
